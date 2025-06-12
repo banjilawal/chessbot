5de0bb915baadba386c74c6556b2bdf8dde25ad2
Banji Lawal <banji.k.lawal@gmail.com>
2025-06-12 12:48:25 -0500

    Wrote separate class for unit testing GameBoarSquare.

--- test/game/board/test_board.py
+++ test/game/board/test_board.py
@@ -1,50 +1,6 @@
 import unittest
 
-from game.board.board_square import GameBoardSquare
 from game.board.game_board import GameBoard
-
-
-class TestGameBoardSquare(unittest.TestCase):
-    def test_init(self):
-        """Test basic initialization with valid values"""
-        square = GameBoardSquare(id=1, row=0, column=0)
-        self.assertIsNotNone(square)
-        self.assertEqual(square.id, 1)
-        self.assertEqual(square.row, 0)
-        self.assertEqual(square.column, 0)
-
-    # ID tests
-    def test_valid_id(self):
-        """Test square creation with valid ID"""
-        squares_cell = GameBoardSquare(id=1, row=0, column=0)
-        self.assertEqual(squares_cell.id, 1)
-
-    def test_invalid_id(self):
-        """Test square creation with invalid ID"""
-        with self.assertRaises(ValueError):
-            GameBoardSquare(id=-1, row=0, column=0)
-
-    # Row tests
-    def test_valid_row(self):
-        """Test square creation with valid row"""
-        squares_cell = GameBoardSquare(id=1, row=0, column=0)
-        self.assertEqual(squares_cell.row, 0)
-
-    def test_invalid_row(self):
-        """Test square creation with invalid row"""
-        with self.assertRaises(ValueError):
-            GameBoardSquare(id=1, row=-1, column=0)
-
-    # Column tests
-    def test_valid_column(self):
-        """Test square creation with valid column"""
-        squares_cell = GameBoardSquare(id=1, row=0, column=0)
-        self.assertEqual(squares_cell.column, 0)
-
-    def test_invalid_column(self):
-        """Test square creation with invalid column"""
-        with self.assertRaises(ValueError):
-            GameBoardSquare(id=1, row=0, column=-1)
 
 
 class TestGameBoard(unittest.TestCase):
