9ded47e009e259d02e95b0e9f46a02a4e8bb5290
Banji Lawal <banji.k.lawal@gmail.com>
2025-06-13 02:11:07 -0500

    Renamed GameFigure unit tests for clarity.

--- test/game/figure/test_game_figure.py
+++ test/game/figure/test_game_figure.py
@@ -3,7 +3,8 @@ from unittest.mock import Mock
 
 
 from exception.exception import NullSquareEntryError, InvalidIdError, InvalidFigureHeightError, \
-    OccupiedSquareEntryError, SelfOccupiedSquareError, InvalidFigureLengthError, NoSquareToLeaveError
+    OccupiedSquareEntryError, SelfOccupiedSquareError, InvalidFigureLengthError, NoSquareToLeaveError, \
+    FigureAreaBelowLimitError
 from game.board.board_square import GameBoardSquare
 from game.figure.game_figure import GameFigure
 
@@ -19,58 +20,26 @@ class TestGameFigure(unittest.TestCase):
         self.mock_figure = Mock(name="MockFigure")
         self.figure = GameFigure(_id=1, _length=2, _height=3)
 
-    def test_enter_square_updates_square(self):
-        self.figure.enter_square(self.mock_square)
-        self.assertIs(self.figure.square, self.mock_square)
+    def test_constructing_figure_with_valid_id(self):
+        """Test figure creation with valid ID"""
+        figure = GameFigure(_id=1, _length=2, _height=3)
+        self.assertEqual(figure.id, 1)
 
-    def test_enter_square_updates_occupant(self):
-        self.figure.enter_square(self.mock_square)
-        self.assertIs(self.mock_square.occupant, self.figure)
+    def test_constructing_figure_with_invalid_id_raises_error(self):
+        """Test figure creation with invalid ID"""
+        with self.assertRaises(InvalidIdError):
+            GameFigure(_id=-1, _length=2, _height=2)
 
-    def test_enter_null_square_raises_error(self):
-        with self.assertRaises(NullSquareEntryError):
-            self.figure.enter_square(None)
-
-    def test_enter_square_occupied_by_self_raises_error(self):
-        self.figure._square = self.mock_square
-        self.mock_square._occupant = self.figure
-        with self.assertRaises(SelfOccupiedSquareError):
-            self.figure.enter_square(self.mock_square)
-
-    def test_enter_square_already_occupied_raises_error(self):
-        self.mock_figure._square = self.mock_square
-        self.mock_square._occupant = self.mock_figure
-        with self.assertRaises(OccupiedSquareEntryError):
-            self.figure.enter_square(self.mock_square)
-
-    def test_leave_square_sets_square_occupant_null(self):
-        self.mock_square._occupant = self.figure
-        self.figure._square = self.mock_square
-        self.figure.leave_square()
-        self.assertIsNone(self.mock_square.occupant)
-
-    def test_leave_square_sets_square_owned_by_figure_null(self):
-        self.mock_square._occupant = self.figure
-        self.figure._square = self.mock_square
-
-        self.figure.leave_square()
-        self.assertIsNone(self.figure.square)
-
-    def test_leave_square_which_is_null_throws_error(self):
-        self.figure._square = None
-        with self.assertRaises(NoSquareToLeaveError):
-            self.figure.leave_square()
-
-    def test_create_figure_with_valid_length(self):
+    def test_constructing_figure_with_valid_length(self):
         figure = GameFigure(_id=1, _length=2, _height=3)
         self.assertGreaterEqual(figure._length, GameFigure.MINIMUM_LENGTH)
 
-    def test_invalid_length_throws_error(self):
+    def test_invalid_figure_length_throws_error(self):
         """Test figure creation with an invalid length"""
         with self.assertRaises(InvalidFigureLengthError):
             GameFigure(_id=1, _length=0, _height=2)
 
-    def test_create_figure_with_valid_height(self):
+    def test_constructing_figure_with_valid_height(self):
         figure = GameFigure(_id=1, _length=2, _height=3)
         self.assertGreaterEqual(figure._height, GameFigure.MINIMUM_HEIGHT)
 
@@ -79,20 +48,58 @@ class TestGameFigure(unittest.TestCase):
         with self.assertRaises(InvalidFigureHeightError):
             GameFigure(_id=1, _length=2, _height=0)
 
-    def test_create_figure_with_valid_id(self):
-        """Test figure creation with valid ID"""
-        figure = GameFigure(_id=1, _length=2, _height=3)
-        self.assertEqual(figure.id, 1)
-
-    def test_invalid_id_throws_error(self):
-        """Test figure creation with invalid ID"""
-        with self.assertRaises(InvalidIdError):
-            GameFigure(_id=-1, _length=2, _height=2)
-
     def test_area_of_figure_greater_or_equal_minimum_area(self):
         """Test figure area is greater than or equal to the minimum area"""
         figure = GameFigure(_id=1, _length=2, _height=3)
         self.assertGreaterEqual(figure.area(), GameFigure.MINIMUM_AREA)
+
+    def test_area_of_figure_below_minimum_raises_error(self):
+        with self.assertRaises(FigureAreaBelowLimitError):
+            GameFigure(_id=1, _length=GameFigure.MINIMUM_LENGTH, _height=GameFigure.MINIMUM_HEIGHT)
+
+    def test_figure_entering_square_updates_square(self):
+        self.figure.enter_square(self.mock_square)
+        self.assertIs(self.figure.square, self.mock_square)
+
+    def test_square_updates_occupant_on_figure_entered(self):
+        self.figure.enter_square(self.mock_square)
+        self.assertIs(self.mock_square.occupant, self.figure)
+
+    def test_figure_entering_null_square_raises_error(self):
+        with self.assertRaises(NullSquareEntryError):
+            self.figure.enter_square(None)
+
+    def test_figure_entering_square_it_already_occupies_raises_error(self):
+        self.figure._square = self.mock_square
+        self.mock_square._occupant = self.figure
+        with self.assertRaises(SelfOccupiedSquareError):
+            self.figure.enter_square(self.mock_square)
+
+    def test_figure_entering_square_occupied_by_other_figure_raises_error(self):
+        self.mock_figure._square = self.mock_square
+        self.mock_square._occupant = self.mock_figure
+        with self.assertRaises(OccupiedSquareEntryError):
+            self.figure.enter_square(self.mock_square)
+
+    def test_figure_leaving_square_sets_square_occupant_to_null(self):
+        self.mock_square._occupant = self.figure
+        self.figure._square = self.mock_square
+        self.figure.leave_square()
+        self.assertIsNone(self.mock_square.occupant)
+
+    def test_figure_looses_ownership_of_square_on_exit(self):
+        self.mock_square._occupant = self.figure
+        self.figure._square = self.mock_square
+        self.figure.leave_square()
+        self.assertIsNone(self.figure.square)
+
+    def test_figure_leaving_nonexistent_square_raises_error(self):
+        self.figure._square = None
+        with self.assertRaises(NoSquareToLeaveError):
+            self.figure.leave_square()
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
