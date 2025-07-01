f0578fd19e7004d277b882bffde5267e2a16ac4b
Banji Lawal <banji.k.lawal@gmail.com>
2025-06-30 21:56:07 -0500

    Wrote view.BoardView.

--- src/view/board_view.py
+++ src/view/board_view.py
@@ -1,6 +1,7 @@
 import pygame
 
 from model.board.board import Board
+from model.cell.cell import Cell
 
 
 class BoardView:
@@ -10,14 +11,19 @@ class BoardView:
     def __init__(self, board: Board, cell_px: int):
         self.board = board
         self.cell_px = cell_px
+    #
 
-    def draw(self, surface: pygame.Surface):
+    def draw_board(self, surface: pygame.Surface):
         """Draw the board on the given surface."""
         for row_index in range(self.board.row_count):
             for column_index in range(self.board.column_count):
-                cell = self.board.
-                rect = pygame.Rect(col * self.cell_px, row * self.cell_px, self.cell_px, self.cell_px)
-                if cell.is_occupied():
-                    color = cell.occupant.color
-                else:
-                    color = (255, 255, 255)
+                cell = self.board.cells[row_index][column_index]
+                self.draw_cell(surface, cell)
+    #
+
+    def draw_cell(self, surface: pygame.Surface, cell: Cell):
+        x = cell.column * self.cell_px
+        y = cell.row * self.cell_px
+        rectangle = pygame.Rect(x, y, self.cell_px, self.cell_px)
+        pygame.draw.rect(surface, pygame.Color("Gray"), rectangle)
+    #
