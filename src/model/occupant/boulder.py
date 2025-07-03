2cd39470a5ee5f2dfca8482eb7e2f358b14cf7ce
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-02 21:20:14 -0500

    Added TYPE_CHECKING to Occunpant to fix circular dependencies with Cell.

--- src/model/occupant/boulder.py
+++ src/model/occupant/boulder.py
@@ -3,9 +3,6 @@ from dataclasses import dataclass
 from model.occupant.occupant import Occupant
 
 
-# Added model.occupant.EscapePortal which extends Occupant class. Added list of walls to the Board. Renamed
-# model.occupant.Wall to model.occupant.Boulder. The Board has 4 walls so the old name was ambigous and not
-# communicating the item's intent."
 
 @dataclass
 class Boulder(Occupant):
