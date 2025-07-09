38fa85a68797a4bc2919cdf8d68e9dc057a887d8
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-08 21:32:04 -0500

    Created VaultGroup for groups of adjacent cells which can be either in rows, or columns.

--- src/model/vault.py
+++ src/model/vault.py
@@ -1,7 +1,8 @@
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import Optional, List
 
 from common.dimension import Dimension
+from common.direction import Direction
 from model.grid_coordinate import GridCoordinate
 from model.grid_entity import GridEntity
 
@@ -11,3 +12,8 @@ class Vault(GridEntity):
 
     def __init__(self, vault_id: int, coordinate: Optional[GridCoordinate] = None):
         super().__init__(id=vault_id, dimension=Dimension(length=1, height=1), coordinate=coordinate)
+
+@dataclass
+class VaultGroup:
+    growth_direction: Direction
+    vaults: List[Vault] = field(default_factory=list)
