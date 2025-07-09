1d06e72308a9eab36941005240ae4f1cb4400dd1
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-09 10:20:41 -0500

    Restricted crates to be 1x1 squares.

--- src/model/crate.py
+++ src/model/crate.py
@@ -9,8 +9,8 @@ from model.grid_entity import GridEntity
 @dataclass
 class Crate(GridEntity):
 
-    def __init__(self, crate_id: int, length: int, coordinate: Optional[GridCoordinate] = None):
-        super().__init__(id=crate_id, dimension=Dimension(length=length, height=1), coordinate=coordinate)
+    def __init__(self, crate_id: int, coordinate: Optional[GridCoordinate] = None):
+        super().__init__(id=crate_id, dimension=Dimension(length=1, height=1), coordinate=coordinate)
 
     def move_left(self, distance: int):
         new_coordinate = GridCoordinate(row=self.coordinate.row, column=self.coordinate.column - distance)
