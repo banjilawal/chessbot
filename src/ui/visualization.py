412c540354928fbe9c3965a6d7114cd8fd357984
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-10 18:04:58 -0500

    Wrote game_entry_point. This handles imports so the runner can find all the executbales in sources_rootu and the poject-root.

---
+++ src/ui/visualization.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass, field
+
+import pygame
+from typing import TYPE_CHECKING, List
+
+from model.grid_entity import GridEntity
+
+if TYPE_CHECKING:
+    from model.board import Board
+    from model.vault import HorizontalMover
+
+@dataclass
+class Visualizer:
+    board: Board
+    entities: List[GridEntity] = field(default_factory=list)
+
+    cell_px: int = 60
+    border_px: int = 2
+    screen_width: int = 800
+    screen_height: int = 800
+
+    background_color: pygame.Color = pygame.Color("black")
+    cell_color: pygame.Color = pygame.Color("white")
+    horizontal_mover_color: pygame.Color = pygame.Color("red")
+
+    def __post_init__(self):
+        self.screen_width = self.board.dimension.length * self.cell_px + self.border_px * 2
+        self.screen_height = self.board.dimension.height * self.cell_px + self.border_px * 2
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Podscape")
+        self.font = pygame.font.SysFont("monospace", 15)
+
+    def draw_grid(self, board: 'Board'):
+        self.screen.fill(self.background_color)
+        for row in range(self.board.dimension.height):
+            for col in range(self.board.dimension.length):
+                cell_rect = pygame.Rect(
+                    col * self.cell_px + self.border_px,
+                    row * self.cell_px + self.border_px,
+                    self.cell_px,
+                    self.cell_px
+                )
+                # Draw filled rectangle
+                pygame.draw.rect(self.screen, self.cell_color, cell_rect)
+                # Draw outlined rectangle
+                pygame.draw.rect(self.screen, self.background_color, cell_rect, 1)
+
+    def update_display(self):
+        self.draw_grid()
+        pygame.display.flip()
+
+    def close(self):
+        pygame.quit()
+
+
+
