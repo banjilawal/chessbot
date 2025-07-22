98093caf3b0ef6c268a29d2375bea8421b25c2cb
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-22 11:54:27 -0500

    Fixed imports, semantic errors in MoveRule subclasses.2) Added constructors to MoveRule subclasses.3) MoveRule subclass attribute names match those in super class.

--- src/chess/movement/movement.py
+++ src/chess/movement/movement.py
@@ -6,12 +6,15 @@ from chess.common.geometry import Coordinate
 class MoveRule(ABC):
     origin: Coordinate
     destination: Coordinate
-    
+
     def __self__(self, origin: Coordinate, destination: Coordinate):
         if origin is None:
             raise ValueError("Origin cannot be None")
         if destination is None:
             raise ValueError("Destination cannot be None")
+        if origin == destination:
+            print("Cannot move if origin and destination are the same")
+            return
         self._origin = origin
         self._destination = destination
 
@@ -44,17 +47,25 @@ class MoveRule(ABC):
     def is_valid_move(self, origin: Coordinate, destination: Coordinate) -> bool:
         pass
 
+
 class VerticalMoveRule(MoveRule):
     """Y changes while X stays the same."""
-    def is_valid_move(self, start: GridCoordinate, end: GridCoordinate) -> bool:
+    def __init__(self, origin: Coordinate, destination: Coordinate):
+        super().__init__(origin, destination)
+
+    def is_valid_move(self, origin: Coordinate, destination: Coordinate) -> bool:
         # Same column, row changes
-        return start.column == end.column and start.row != end.row
+        return origin.column == destination.column and origin.row != destination.row
+
 
 class HorizontalMoveRule(MoveRule):
     """X changes while Y stays the same."""
-    def is_valid_move(self, start: GridCoordinate, end: GridCoordinate) -> bool:
+    def __init__(self, origin: Coordinate, destination: Coordinate):
+        super().__init__(origin, destination)
+
+    def is_valid_move(self, origin: Coordinate, destination: Coordinate) -> bool:
         # Same row, column changes
-        return start.row == end.row and start.column != end.column
+        return origin.row == destination.row and origin.column != destination.column
 
 
 class DiagonalMoveRule(MoveRule):
@@ -63,8 +74,11 @@ class DiagonalMoveRule(MoveRule):
     forward Xj, Yj <= Xi, Yi = Xi-1, Yi+1
     backward Xj, Yj>=Xi, Yi = Xi+1, Yi+1
     """
-    def is_valid_move(self, start: GridCoordinate, end: GridCoordinate) -> bool:
+    def is_valid_move(self, origin: Coordinate, destination: Coordinate) -> bool:
         # Row and column difference equal (non-zero)
-        return abs(end.row - start.row) == abs(end.column - start.column) and start != end
+        return (
+            origin != destination and
+                abs(origin.row - destination.row) == abs(destination.column - origin.column)
+        )
 
 
