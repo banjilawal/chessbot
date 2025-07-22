0015b6cad38f821869f4dc33eec42f8882e722f8
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-22 03:07:00 -0500

    Renamed HomeOrientation TeamHome. Probably only need get_enemy_home() method.

--- src/chess/team/home.py
+++ src/chess/team/home.py
@@ -3,29 +3,29 @@ from enum import Enum
 from chess.common.config import BOARD_DIMENSION
 
 
-class HomeOrientation(Enum):
+class TeamHome(Enum):
     NORTH = "North"
     SOUTH = "South"
 
     def first_home_row(self):
-        if self == HomeOrientation.NORTH:
+        if self == TeamHome.NORTH:
             return 0
         return BOARD_DIMENSION - 1
 
     def second_home_row(self):
-        if self == HomeOrientation.SOUTH:
+        if self == TeamHome.SOUTH:
             return 1
         return BOARD_DIMENSION - 2
 
     def get_enemy_home(self):
-        if self == HomeOrientation.NORTH:
-            return HomeOrientation.SOUTH
-        return HomeOrientation.NORTH
+        if self == TeamHome.NORTH:
+            return TeamHome.SOUTH
+        return TeamHome.NORTH
 
     def get_enemy_home_row(self):
         return self.get_enemy_home().first_home_row()
 
     def step_toward_enemy(self):
-        if self == HomeOrientation.NORTH:
+        if self == TeamHome.NORTH:
             return -1
         return 1
