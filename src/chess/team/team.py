dbc1402cf5c5e359d6ac89204892627a4698ad75
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-22 05:41:28 -0500

    Removed setters in Team class. Decreased the number of fields in Team constructor.

--- src/chess/team/team.py
+++ src/chess/team/team.py
@@ -7,32 +7,29 @@ from chess.team.home import TeamHome
 from podscape.constants import GameColor
 
 class Team:
-    team_id: int
-    color: GameColor
-    home: TeamHome
-    captives:List[ChessPiece] = field(default_factory=list)
-    piece_registry: Dict[ChessPieceConfig, Dict[int, Optional[ChessPiece]]] = field(init=False)
+    _id: int
+    _color: GameColor
+    _home: TeamHome
+    _captives:List[ChessPiece] = field(default_factory=list)
+    _piece_registry: Dict[ChessPieceConfig, Dict[int, Optional[ChessPiece]]] = field(init=False)
 
-    def __post_init__(self):
-        object.__setattr__(self, 'home_row', 6 if self.color == GameColor.RED else 1)
-        object.__setattr__(self, 'goal_row', 0 if self.color == GameColor.RED else 7)
-        object.__setattr__(self, 'advancement_direction', -1 if self.color == GameColor.RED else +1)
+    def __init__(self, team_id: int, color: GameColor, home:TeamHome):
+        self._id = team_id
+        self._color = color
+        self._home = home
 
-        initial_pieces = self._generate_initial_registry()
-        object.__setattr__(self, 'piece_registry', initial_pieces)
-
-    def _generate_initial_registry(self) -> Dict[str, Optional[ChessPiece]]:
-        ranks = {
-            "pawn": 8,
-            "rook": 2,
-            "knight": 2,
-            "bishop": 2,
-            "queen": 1,
-            "king": 1,
-        }
-        registry = {}
-        for rank_name, count in ranks.items():
-            for i in range(1, count + 1):
-                piece_id = f"{rank_name}_{i}" if count > 1 else rank_name
-                registry[piece_id] = ChessPiece(team=self, rank_name=rank_name, chess_piece_id=piece_id)
-        return registry
+    @property
+    def id(self) -> int:
+        return self._id
+    @property
+    def color(self) -> GameColor:
+        return self._color
+    @property
+    def home(self) -> TeamHome:
+        return self._home
+    @property
+    def captives(self) -> List[ChessPiece]:
+        return List(self._captives)
+    @property
+    def piece_registry(self) -> Dict[int, Optional[ChessPiece]]:
+        return Dict(self._piece_registry)
