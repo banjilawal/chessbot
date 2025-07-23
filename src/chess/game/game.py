ea152c505142b7401e6b4eca4a2949be7fc40242
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-23 05:34:36 -0500

    Added fields and constructor to chess.game.Game.

--- src/chess/game/game.py
+++ src/chess/game/game.py
@@ -1,2 +1,34 @@
+from chess.board.chess_board import ChessBoard
+from chess.game.record.turn_record import TurnRecord
+from chess.team.team import Team
+
+
 class Game:
-    s
+    _board = ChessBoard
+    _white_team = Team
+    _black_team = Team
+    _game_history: list[TurnRecord]
+
+    def __init__(self, board, white_team, black_team):
+        self._board = board
+        self._white_team = white_team
+        self._black_team = black_team
+
+    @property
+    def get_board(self):
+        return self._board
+
+
+    @property
+    def get_white_team(self):
+        return self._white_team
+
+
+    @property
+    def get_black_team(self):
+        return self._black_team
+
+
+    @property
+    def get_game_history(self):
+        return self._game_history
