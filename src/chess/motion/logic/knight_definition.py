95d1a896cb663a93302a7348b5c165a222b3a41c
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-24 17:48:53 -0500

    Renamed chess.motion.logic.KnightDefinition KnightPattern. Updated its implementation of abstract class.

--- src/chess/motion/logic/knight_definition.py
+++ src/chess/motion/logic/knight_definition.py
@@ -2,10 +2,9 @@ from chess.common.geometry import Coordinate
 from chess.motion.logic.geomtery_pattern import GeometryPattern
 
 
-class KnightDefinition(GeometryPattern):
-    def __init__(self, definition_id: int, title: str):
-        super().__init__(definition_id, title)
+class KnightPattern(GeometryPattern):
 
-    def line_fits_definition(self, origin: Coordinate, destination: Coordinate) -> bool:
+    @staticmethod
+    def matches(origin: Coordinate, destination: Coordinate) -> bool:
         return (abs(origin.row - destination.row) == 3 and
             abs(origin.column - destination.column) == 1)
