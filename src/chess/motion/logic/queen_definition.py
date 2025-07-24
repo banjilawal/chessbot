ee369b0131e2c3b0d416caf64754302c4211d66f
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-24 18:50:48 -0500

    Forgot to add DiagonalPattern into QueenPattern.

--- src/chess/motion/logic/queen_definition.py
+++ src/chess/motion/logic/queen_definition.py
@@ -1,4 +1,5 @@
 from chess.common.geometry import Coordinate
+from chess.motion.logic.diagonal_pattern import DiagonalPattern
 from chess.motion.logic.geomtery_pattern import GeometryPattern
 from chess.motion.logic.horizontal_pattern import HorizontalPattern
 from chess.motion.logic.vertical import VerticalDefinition
@@ -8,5 +9,8 @@ class QueenPattern(GeometryPattern):
 
     @staticmethod
     def matches(origin: Coordinate, destination: Coordinate) -> bool:
-        return (HorizontalPattern.matches(origin, destination) or
-                VerticalDefinition.matches(origin, destination))
+        return (
+            HorizontalPattern.matches(origin, destination) or
+            VerticalDefinition.matches(origin, destination) or
+            DiagonalPattern.matches(origin, destination)
+        )
