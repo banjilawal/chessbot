2c80eea038d8631e5a346a8ed87623f71231c0fa
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-26 07:23:23 -0500

    Moved chess.common.piece to chess.piece.Label.

--- src/chess/piece/captivity_status.py
+++ src/chess/piece/captivity_status.py
@@ -1,6 +1,5 @@
 from enum import auto, Enum
 
-
 class CaptivityStatus(Enum):
     FREE = auto()
     PRISONER = auto
---
+++ src/chess/piece/label.py
@@ -0,0 +1,27 @@
+class Label:
+    _letters: str
+    _number: int
+    def __init__(self, letters: str, number: int):
+        self._letters = letters
+        self._number = number
+
+    @property
+    def letters(self) -> str:
+        return self._letters
+
+    @property
+    def number(self) -> int:
+        return self._number
+
+    @property
+    def label(self) -> str:
+        return f"{self._letters}-{self._number}"
+
+    def __eq__(self, other):
+        if other is self:
+            return True
+        if other is None:
+            return False
+        if not isinstance(other, Label):
+            return False
+        return self._letters == other.letters and self._number == other.number
