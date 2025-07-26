4a71e12e01f0bcd661b91f1686b7b3cdbb4a8317
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-26 13:35:43 -0500

    Removed Column generator from chess.factory.IdEmitter. It was bugggy and not worth fixing when I could gnerate column names in GridGenerator. Column names only need to be generated once and they are only letters A-H.

--- src/chess/factory/emit.py
+++ src/chess/factory/emit.py
@@ -4,37 +4,29 @@ class IdEmitter:
         self._coordinate_id = 0
         self._square_id = 0
         self._piece_id = 0
-        self._ascii_value = ord('a')
-        self._column_name = chr(self._ascii_value)
 
-        self._column_index = 0
 
     @property
     def player_id(self) -> int:
         self._player_id += 1
         return self._player_id
 
+
     @property
     def coordinate_id(self) -> int:
         self._coordinate_id += 1
         return self._coordinate_id
+
 
     @property
     def square_id(self) -> int:
         self._square_id += 1
         return self._square_id
 
+
     @property
     def piece_id(self) -> int:
         self._piece_id += 1
         return self._piece_id
 
-    @property
-    def column_name(self) -> str:
-        print("1 col_index:", self._column_index," ", self._column_name)
-        self._column_name = chr(self._ascii_value)
-        self._ascii_value += 1
-        if self._ascii_value > 7:
-            self._ascii_value = 0
-        return self._column_name
 id_emitter = IdEmitter()
--- src/chess/factory/grid_builder.py
+++ src/chess/factory/grid_builder.py
@@ -16,30 +16,13 @@ class GridBuilder:
 
     @staticmethod
     def build() -> List[Square]:
-        column_name = id_emitter.column_name
-        squares: List[Square] = []
 
-        # columns = []
-        # ascii_value = ord('A')
-        # for i in range(COLUMN_SIZE):  # Only 8 columns: A to H
-        #     print(i, chr(ascii_value))
-        #     column_name = chr(ascii_value)
-        #     column = Column(i + 1, column_name)
-        #     columns.append(column)
-        #     print(column.name + "-"+ str(column.id))
-        #     ascii_value += 1
-        #
-        # rows = []
-        # for i in range(ROW_SIZE):
-        #     row = Row(i + 1)
-        #     rows.append(row)
-        #     # column = Column(i + 1, id_emitter.column_name)
-        #     # print(i,  column.name)
-        #     # columns.append(column)
+        squares: List[Square] = []
 
         for i in range(ROW_SIZE):
             row = Row(row_id=(i + 1))
             ascii_value = ord('A')
+
             for j in range(COLUMN_SIZE):
                 column = Column(j + 1, chr(ascii_value))
                 coordinate = Coordinate(coordinate_id=id_emitter.coordinate_id, row=row, column=column)
