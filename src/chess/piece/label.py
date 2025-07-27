1e1eea3cdbc2440cd7f75aaa141a6c4eded6c775
Banji Lawal <banji.k.lawal@gmail.com>
2025-07-27 14:46:28 -0500

    Removed Label.label() -> str: Its really bad form to have a method named afte rthe class. Its work was done by toString anyway. Updated Label.toString().

--- src/chess/piece/label.py
+++ src/chess/piece/label.py
@@ -1,9 +1,11 @@
 class Label:
     _letters: str
     _number: int
+
     def __init__(self, letters: str, number: int):
         self._letters = letters
         self._number = number
+
 
     @property
     def letters(self) -> str:
@@ -13,11 +15,6 @@ class Label:
     @property
     def number(self) -> int:
         return self._number
-
-
-    @property
-    def label(self) -> str:
-        return f"{self._letters}-{self._number}"
 
 
     def __eq__(self, other):
@@ -34,4 +31,4 @@ class Label:
         return hash((self._letters, self._number))
 
     def __str__(self):
-        return f"{self._letters}-{self._number}"
+        return f"{self._letters}{self._number}"
