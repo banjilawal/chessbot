6aa54c21bbc974a1ea003fcbbeadb85504db9b2c
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-08 13:11:59 -0500

    Added constructor and getters to chess.match.Match.

--- src/chess/match/match.py
+++ src/chess/match/match.py
@@ -9,3 +9,25 @@ class Match:
     _arena: Arena
     _winner: Owner
     _game_Log: List[CoordinateStack]
+
+    def __init__(self, match_id: int, arena: Arena):
+        self._id = match_id
+        self._arena = arena
+        self._winner = None
+        self._game_log = []
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def arena(self) -> Arena:
+        return self._arena
+
+    @property
+    def winner(self) -> Owner:
+        return self._winner
+
+    @property
+    def game_log(self) -> List[CoordinateStack]:
+        return self._game_log
