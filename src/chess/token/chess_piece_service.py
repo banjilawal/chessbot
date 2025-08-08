88dafdae3f709e387aa5659961e4c1c977f6cf9b
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-08 17:42:58 -0500

    1) Wrote chess.token.ChessPieceRepo. 2) Wrote chess.token.ChessService.

---
+++ src/chess/token/chess_piece_service.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+from chess.owner.model.owner import Owner
+from chess.team.element.piece import ChessPiece
+from chess.token.chess_piece_repo import ChessPieceRepo
+
+
+class ChessPieceService:
+    _repo: ChessPieceRepo
+
+    def __init__(self, repo: ChessPieceRepo):
+        self._repo = repo
+
+
+    def add_chess_piece(self, chess_piece: ChessPiece):
+        self._repo.add(chess_piece)
+
+    def find_chess_piece_by_id(self, chess_piece_id: int) -> Optional[ChessPiece]:
+        return self._repo.find(chess_piece_id)
+
+    def find_by_owner(self, owner: Owner) -> List[ChessPiece]:
+        return self.repo.filter_by_owner_id(owner.id)
