d4aca669be7933096bc0a108e1d343638639d1c9
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-09 17:15:19 -0500

    Wrote GreedyDecisionEngine.

--- src/chess/engine/decision/decision_engine.py
+++ src/chess/engine/decision/decision_engine.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
+from chess.config.rank_config import RankConfig
 from chess.engine.analyze.board_analysis import BoardAnalysis
 from chess.engine.analyze.scout_report_analysis import ScoutReportAnalysis
 from chess.geometry.coordinate.coordinate import Coordinate
@@ -8,12 +9,24 @@ from chess.geometry.coordinate.coordinate import Coordinate
 
 class DecisionEngine(ABC):
     _board_analysis: BoardAnalysis
+    _max_capture_value: int
 
     def __init__(self, board_analysis: BoardAnalysis):
         self._board_analysis = board_analysis
+        self._max_capture_value = RankConfig.QUEEN.capture_value
+
+
+    @property
+    def board_analysis(self) -> BoardAnalysis:
+        return self._board_analysis
+
+
+    @property
+    def max_capture_value(self) -> int:
+        return self._max_capture_value
 
 
     @abstractmethod
-    def decide_destination() -> Optional[Coordinate]:
+    def decide_destination(self) -> Optional[Coordinate]:
         pass
 
--- src/chess/engine/decision/greedy_decision_engine.py
+++ src/chess/engine/decision/greedy_decision_engine.py
@@ -1,87 +1,83 @@
 from typing import List, Optional
 
 from chess.config.rank_config import RankConfig
-from chess.engine.analyze.scout_report_analysis import ScoutReportAnalysis
+from chess.engine.analyze.board_analysis import BoardAnalysis
 from chess.engine.decision.decision_engine import DecisionEngine
 from chess.geometry.coordinate.coordinate import Coordinate, CartesianDistance
-from chess.rank.queen_rank import QueenRank
-from chess.token.piece import ChessPiece
 
 
-class GreedyEngine(DecisionEngine):
-
-    @staticmethod
-    def decide_destination(board_analysis: List[ScoutReportAnalysis]) -> Optional[Coordinate]:
+class GreedyDecisionEngine(DecisionEngine):
 
 
-    @staticmethod
-    def _decision_helper(board_analysis: List[ScoutReportAnalysis]) -> Optional[Coordinate]:
-        if len(board_analysis.ca) == 0:
-            return None
-        best_capture_report = GreedyEngine._select_best_capture_report(board_analysis)
-        best_obstruction_report = GreedyEngine._select_best_obstruction_report(board_analysis)
-        best_vacant_square_report = GreedyEngine._select_furthest_vacant_square_report(board_analysis)
+    def __init__(self, board_analysis: BoardAnalysis):
+        super().__init__(board_analysis)
 
-        if best_capture_report is not None:
-            return best_capture_report.chess_piece.coordinate_stack.current_coordinate()
-        elif best_obstruction_report is not None:
-            return best_obstruction_report.chess_piece.coordinate_stack.current_coordinate()
-        elif best_vacant_square_report is not None:
-            return best_vacant_square_report.chess_piece.coordinate_stack.current_coordinate()
+
+    def decide_destination(self) -> Optional[Coordinate]:
+        return self._decision_helper()
+
+
+    def _decision_helper(self) -> Optional[Coordinate]:
+
+        if self._board_analysis.enemy_report_count > 0:
+            return self._select_best_capture_report()
+        elif self._board_analysis.vacancy_report_count > 0:
+            return self._select_furthest_vacant_square_report()
+        elif self._board_analysis.obstruction_report_count > 0:
+            return self._select_best_obstruction_report()
         else:
             return None
 
 
-    @staticmethod
-    def _select_best_capture_report(
-        board_analysis: List[ScoutReportAnalysis]
-    ) -> Optional[ScoutReportAnalysis]:
-
-        highest_capture_value = RankConfig.QUEEN.capture_value
-        min_capture_value_range = RankConfig.QUEEN.capture_value
-
+    def _select_best_capture_report(self) -> Optional[Coordinate]:
         best_report = None
+        min_capture_diff = self._max_capture_value
 
-        for analysis in board_analysis:
-            current_capture_value_diff = highest_capture_value - analysis.enemies[0].rank.capture_value
-            if current_capture_value_diff < min_capture_value_range:
+        for analysis in self._board_analysis:
+            current_capture_value = analysis.enemies[0].rank.capture_value
+
+            if self._max_capture_value - current_capture_value < min_capture_diff:
                 best_report = analysis
+
+        if best_report is not None:
+            return best_report.enemies[0].coordinate_stack.current_coordinate()
         return best_report
 
 
-    @staticmethod
-    def _select_furthest_vacant_square_report(
-        board_analysis: List[ScoutReportAnalysis]
-    ) -> Optional[ScoutReportAnalysis]:
+    def _select_furthest_vacant_square_report(self) -> Optional[Coordinate]:
 
         max_distance = 0
         best_report = None
 
-        for analysis in board_analysis:
+        for analysis in self._board_analysis:
 
             square = analysis.vacant_squares[0]
             origin = analysis.chess_piece.coordinate_stack.current_coordinate()
 
             if CartesianDistance(origin, square.coordinate).distance > max_distance:
                 best_report = analysis
+
+        if best_report is not None:
+            return best_report.vacant_squares[0].coordinate
         return best_report
 
 
     @staticmethod
-    def _select_best_obstruction_report(
-        board_analysis: List[ScoutReportAnalysis]
-    ) -> Optional[ScoutReportAnalysis]:
+    def _select_best_obstruction_report(self) -> Optional[Coordinate]:
 
         max_distance = 0
         best_report = None
 
-        for analysis in board_analysis:
+        for analysis in self._board_analysis:
             coordinate = analysis.obstructions[0].blocked_coordinate
             origin = analysis.chess_piece.coordinate_stack.current_coordinate()
 
-            if CartesianDistance(origin, coordinate).distance >max_distance:
+            if CartesianDistance(origin, coordinate).distance > max_distance:
                 best_report = analysis
-        return best_report
+
+        if best_report is not None:
+            return best_report.obstructions[0].blocked_coordinate
+        return None
 
 
 
