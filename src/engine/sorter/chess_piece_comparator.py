c59afc7bcabac7ba0781481fd2e3aaed99e325ea
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-08 23:16:09 -0500

    Fixed comparators.

--- src/engine/sorter/chess_piece_comparator.py
+++ src/engine/sorter/chess_piece_comparator.py
@@ -1,11 +1,21 @@
 from chess.rank.rank import Rank
-
-
-
+from chess.token.piece import ChessPiece
 
 
 class ChessPieceComparator:
 
     @staticmethod
-    def compare(a: Rank, b: Rank):
-        return a.capture_value - b.capture_value
+    def compare(a: ChessPiece, b: ChessPiece):
+        if not a.is_enemy(b):
+            return a.capture_value - b.capture_value
+        return 0
+
+class SquareOccupantComparator:
+    def __init__(self, piece):
+        self.piece = piece
+
+    def compare(self, square1, square2):
+        if self.piece.color == 'white':
+            return square1.y - square2.y
+        else:
+            return square2.y - square1.y
--- src/engine/sorter/scout_report_sorter.py
+++ src/engine/sorter/scout_report_sorter.py
@@ -1,11 +1,16 @@
 from typing import List
 
 from engine.scout.scout_report import ScoutReport
+from engine.sorter.square_comparator import SquareComparator
 
 
 class ScoutReportSorter:
 
     @staticmethod
-    def receive_unsorted_eports(comparator: SquareComparator, reports: List[ScoutReport]) -> List[ScoutReport]:
+    def receive_unsorted_reports(
+            comparator: SquareComparator,
+            reports: List[ScoutReport]
+    ) -> List[ScoutReport]:
+        pass
 
 
