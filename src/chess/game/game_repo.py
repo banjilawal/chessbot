7fbeb3d75c80351ebcbf75ba2c5ac9728ea28086
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-08 19:53:10 -0500

    Finished GameRepo.

---
+++ src/chess/game/game_repo.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+
+from chess.arena.arena import Arena
+from chess.game.game import Game
+from chess.owner.model.owner import Owner
+
+
+class GameRepo:
+    _chess_pieces: List[Game]
+
+    def __init__(self,):
+        self._games = []
+
+
+    def add(self, game: Game):
+        if game not in self._games and game is not None:
+            self._chess_pieces.append(game)
+
+
+    def find(self, game_id: int) -> Optional[Game]:
+        for game in self._games:
+            if game.id == game_id:
+                return game
+        return None
+
+
+    def filter_by_winner(self, winner: Owner) -> List[Game]:
+        matches: List[Game] = []
+
+        for game in self._games:
+            if game.winner == winner and game not in matches:
+                matches.append(game)
+        return matches
+
+
+    def filter_by_arena(self, arena: Arena) -> List[Game]:
+        matches: List[Game] = []
+
+        for game in self._games:
+            if game.arena == arena and game not in matches:
+                matches.append(game)
+        return matches
+
---
+++ src/chess/game/game_service.py
@@ -0,0 +1,15 @@
+class ChessPieceService:
+    _repo: ChessPieceRepo
+
+    def __init__(self, repo: ChessPieceRepo):
+        self._repo = repo
+
+
+    def add_chess_piece(self, chess_piece: ChessPiece):
+        self._repo.add(chess_piece)
+
+    def find_chess_piece_by_id(self, chess_piece_id: int) -> Optional[ChessPiece]:
+        return self._repo.find(chess_piece_id)
+
+    def find_by_owner(self, owner: Owner) -> List[ChessPiece]:
+        return self._repo.filter_by_owner_id(owner.id)
