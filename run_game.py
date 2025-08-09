750f807ed6cc1bc526878a989c4128648f3b4fe9
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-08 19:45:20 -0500

    1) Renamed chess.game.Match class to Game.2) Created GameRepo and GameService classes.

--- run_game.py
+++ run_game.py
@@ -12,10 +12,10 @@ def main():
         # launch_game(arena)
 
     except Exception as e:
-        logger.critical(f"Match failed: {e}", exc_info=True)
+        logger.critical(f"Game failed: {e}", exc_info=True)
         raise
     finally:
-        logger.info("Match session ended")
+        logger.info("Game session ended")
 
 
 
--- src/chess/game/game.py
+++ src/chess/game/game.py
@@ -4,7 +4,7 @@ from chess.arena.arena import Arena
 from chess.geometry.coordinate.coordinate_stack import CoordinateStack
 from chess.owner.model.owner import Owner
 
-class Match:
+class Game:
     _id: int
     _arena: Arena
     _winner: Owner
