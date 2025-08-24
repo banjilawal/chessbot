c8fd1c47b6de93ea72d69b3e185da42379e36f3c
Banji Lawal <banji.k.lawal@gmail.com>
2025-08-24 10:08:35 -0500

    1) Wrote tests.unit.chees.token.chess_piece_test.ChessPieceTest skeleton.2) Wrote tests.unit.chess.geometry.coordinate.CoordinateTest.

---
+++ tests/integration/chess/token/chess_piece_test.py
@@ -0,0 +1,53 @@
+class ChessPieceTest:
+
+    _id: int
+    _name: str
+    _team: 'Team'
+    _rank: 'Rank'
+    _captor: 'ChessPiece'
+    _status: MobilityStatus
+    _obstructions: List[Obstruction]
+    _coordinate_stack: CoordinateStack
+
+    def test_chess_piece_id_is_positive(self):
+        # Test code for positive ID
+        pass
+    def text_negative_chess_piece_throws_exception(self):
+        # Test code for negative ID exception
+        pass
+
+    def test_chess_piece_id_not_null(self):
+        # Test code for non-null ID
+        pass
+    def text_null_chess_piece_throws_exception(self):
+        # Test code for null ID exception
+        pass
+
+    def chess_piece_name_not_empty(self):
+        # Test code for non-empty name
+        pass
+    def text_empty_name_throws_exception(self):
+        # Test code for empty name exception
+        pass
+
+    def chess_piece_team_not_null(self):
+        # Test code for non-null team
+        pass
+    def text_null_team_throws_exception(self):
+        # Test code for null team exception
+        pass
+
+    def chess_piece_rank_not_null(self):
+        # Test code for non-null rank
+        pass
+    def text_null_rank_throws_exception(self):
+        # Test code for null rank exception
+        pass
+
+    def test_chess_piece_movement(self):
+        # Test code for chess piece movement
+        pass
+
+    def test_chess_piece_capture(self):
+        # Test code for chess piece capture
+        pass
