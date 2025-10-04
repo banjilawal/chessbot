0b84bc171efa8ceca2cc45cbfc5ef817154245b8
Banji Lawal <banji.k.lawal@gmail.com>
2025-10-04 15:32:16 -0500

    1) Added __init__.py to chess.arena.2) Added module section to chess.arena.Arena.3) Created ArenaBuilder.4) Added modulde information to chess.arena.Exception.5) Added InvalidArena NullArena and ArenaFBuildFailed exceptions.6) Retstructed arena.exception module.7) Added HOW TO IMPORT section to __init__.template.

---
+++ src/chess/arena/builder.py
@@ -0,0 +1,64 @@
+# chess/board/builder.py
+
+"""
+Module: `chess.board.builder`
+Author: Banji Lawal
+Created: 2025-10-03
+version: 1.0.0
+
+Contains: BoardBuilder
+Responsibilities: Create `Board` instances
+"""
+
+from typing import List
+
+
+from chess.square import Square
+from chess.board import Board, BoardBuildFailedException
+from chess.system import  BOARD_DIMENSION, Builder, BuildResult
+
+
+
+class BoardBuilder(Builder[Board]):
+    """
+    Responsible for safely constructing `Board` instances.
+    """
+
+    @classmethod
+    def build(cls) -> BuildResult[Board]:
+        """
+        Constructs a new `Board` that works correctly.
+
+        Args:
+            None
+
+        Returns:
+            `BuildResult`[`Board`]: A `BuildResult` containing either:
+                - On success: A valid `Board` instance in the payload
+                - On failure: Error information and error details
+
+        Raises:
+            `BoardBuildFailedException`:`: Wraps any exceptions raised build. These are:
+        """
+        method = "BoardBuilder.build"
+        
+        try:
+            squares: List[List[Square]] = []
+            for i in range(BOARD_DIMENSION):
+                row_squares: List[Square] = []
+                ascii_value = ord('A')
+    
+                for j in range(BOARD_DIMENSION):
+                    name = chr(ascii_value) + str(i + 1)
+                    board = Board(row=i, column=j)
+                    square = Square(name, board)
+    
+                    row_squares.append(square)
+                    ascii_value += 1
+                squares.append(row_squares)
+            return BuildResult(payload=Board(squares=squares))
+
+        except Exception as e:
+            raise BoardBuildFailedException(f"{method}: {e}") from e
+
+
