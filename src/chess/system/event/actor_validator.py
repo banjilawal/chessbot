f3773b60ce1a83a4068a850991aaeac9e18bd8a8
Banji Lawal <banji.k.lawal@gmail.com>
2025-10-17 09:02:03 -0500

    Wrote ActorValidator inteface. Wrote TravelActorValidator.

--- src/chess/system/event/actor_validator.py
+++ src/chess/system/event/actor_validator.py
@@ -1,13 +1,14 @@
 from abc import ABC
 from typing import TypeVar, Generic
 
-from chess.system import ValidationResult
+from chess.system import ValidationResult, Validator
 
 A = TypeVar('A')
 X = TypeVar('X')
 T = TypeVar('T')
 
-class ActorValidator(ABC, Generic[A, X, T]):
+
+class ActorValidator(Generic[A, X, T]):
   """"""
 
   @classmethod
