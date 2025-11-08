4dbfbda26d6afb73bdee3d7bdf8483d06eb1a6c6
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-08 13:21:07 -0600

    wrote GraphBuilder class.

---
+++ src/chess/graph/builder.py
@@ -0,0 +1,36 @@
+# src/chess/graph/builder.py
+
+"""
+Module: chess.graph.builder
+Author: Banji Lawal
+Created: 2025-11-08
+version: 1.0.0
+"""
+
+from typing import Optional
+
+from chess.graph import Graph
+from chess.board import Board, BoardValidator
+from chess.system import Builder, BuildResult, IdValidator, LoggingLevelRouter
+
+
+class GraphBuilder(Builder[Graph]):
+    """"""
+    
+    @classmethod
+    @LoggingLevelRouter.monitor
+    def build(cls, id: int, board: Board) -> BuildResult[Graph]:
+        """"""
+        method = "GraphBuilder.build"
+        try:
+            id_validation = IdValidator.validate(id)
+            if id_validation.is_failure():
+                return BuildResult.failure(id_validation.exception)
+            
+            board_validation = BoardValidator.validate(board)
+            if board_validation.is_failure():
+                return BuildResult.failure(board_validation.exception)
+            
+            return BuildResult.success(Graph(id=id, board=board))
+        except Exception as e:
+            return BuildResult.failure(e)
