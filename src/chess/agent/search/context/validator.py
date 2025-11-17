f5f7cea43bb49e3077609f6f8c82d4b936674fd7
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-17 17:37:39 -0600

    Wrote TeamAgentSearchContextValidator.

--- src/chess/agent/search/context/validator.py
+++ src/chess/agent/search/context/validator.py
@@ -7,28 +7,28 @@ Created: 2025-11-17
 version: 1.0.0
 """
 
+
 from typing import Any, cast
 
-from chess.coord import Coord, CoordValidator
-from chess.system import Validator, IdValidator, NameValidator, ValidationResult, LoggingLevelRouter
-from chess.board import (
-    BoardSearchContext, InvalidBoardSearchContextException, NullBoardSearchContextException,
-    MoreThanOneBoardSearchOptionPickedException, NoBoardSearchOptionSelectedException
+from chess.system import GameColor, Validator, IdentityService, GameColorValidator, ValidationResult, LoggingLevelRouter
+from chess.agent import (
+    AgentTeamSearchContext, InvalidAgentTeamSearchContextException, NullAgentTeamSearchContextException,
+    MoreThanOneAgentTeamSearchOptionPickedException, NoAgentTeamSearchOptionSelectedException
 )
 
 
-class BoardSearchContextValidator(Validator):
+class AgentTeamSearchContextValidator(Validator):
     """
     # ROLE: Validation
 
     # RESPONSIBILITIES:
     1.  Verify a candidate is a AgentTeamSearchContext that meets the application's safety contract before the client
         is allowed to use the AgentTeamSearchContext object.
-    2.  Provide pluggable factories for validating different options separately.
+    2.  Provide pluggable factories for validating different search options separately.
     
     # PROVIDES:
     ValidationResult[AgentTeamSearchContext] containing either:
-        - On success:   Board in the payload.
+        - On success:   AgentTeam in the payload.
         - On failure:   Exception.
 
     # ATTRIBUTES:
@@ -40,21 +40,21 @@ class BoardSearchContextValidator(Validator):
     def validate(
             cls,
             candidate: Any,
-            id_validator: type[IdValidator] = IdValidator,
-            name_validator: type[NameValidator] = NameValidator,
-            coord_validator: type[CoordValidator] = CoordValidator
-    ) -> ValidationResult[BoardSearchContext]:
+            identity_service: IdentityService=IdentityService,
+            color_validator: type[GameColorValidator]=GameColorValidator
+    ) -> ValidationResult[AgentTeamSearchContext]:
         """
         # Action:
         Verifies candidate is a AgentTeamSearchContext in two steps.
-            1.  Test the candidate is a valid SearchBoardContext with a single search option switched on.
+            1.  Test the candidate is a valid SearchAgentTeamContext with a single search option switched on.
             2.  Test the value passed to AgentTeamSearchContext passes its validation contract..
 
         # Parameters:
-          * candidate (Any):                            Object to verify is a Board.
-          * id_validator (type[IdValidator]):           Enforces safety requirements on id-search targets.
-          * name_validator (type[NameValidator]):       Enforces safety requirements on name-search targets.
-          * coord_validator (type[CoordValidator]):     Enforces safety requirements on name-search targets.
+          * candidate (Any):                                Object to verify is a AgentTeam.
+
+          * identity_service (type[IdentityService]):       Enforces safety requirements on name-search targets.
+          
+          * color_validator (type[GameColorValidator]):     Enforces safety requirements on name-search targets.
           
         # Returns:
           ValidationResult[AgentTeamSearchContext] containing either:
@@ -63,61 +63,65 @@ class BoardSearchContextValidator(Validator):
 
         # Raises:
             *   TypeError
-            *   InvalidBoardSearchContextException
-            *   NullBoardSearchContextException
-            *   NoBoardSearchOptionSelectedException
-            *   MoreThanOneBoardSearchOptionPickedException
+            *   NullAgentTeamSearchContextException
+            *   NoAgentTeamSearchOptionSelectedException
+            *   InvalidAgentTeamSearchContextException
+            *   MoreThanOneAgentTeamSearchOptionPickedException
         """
-        method = "BoardSearchContextValidator.validate"
+        method = "AgentTeamSearchContextValidator.validate"
         
         try:
             if candidate is None:
                 return ValidationResult.failure(
-                    NullBoardSearchContextException(f"{method} {NullBoardSearchContextException.DEFAULT_MESSAGE}")
-                )
-            
-            if not isinstance(candidate, BoardSearchContext):
-                return ValidationResult.failure(
-                    TypeError(f"{method}: Expected AgentTeamSearchContext, got {type(candidate).__name__} instead.")
-                )
-            
-            board_search_context = cast(BoardSearchContext, candidate)
-            if len(board_search_context.to_dict() == 0):
-                return ValidationResult.failure(
-                    NoBoardSearchOptionSelectedException(
-                        f"{method}: {NoBoardSearchOptionSelectedException.DEFAULT_MESSAGE}"
+                    NullAgentTeamSearchContextException(
+                        f"{method} {NullAgentTeamSearchContextException.DEFAULT_MESSAGE}"
                     )
                 )
             
-            if len(board_search_context.to_dict()) > 1:
+            if not isinstance(candidate, AgentTeamSearchContext):
                 return ValidationResult.failure(
-                    MoreThanOneBoardSearchOptionPickedException(
-                        f"{method}: {MoreThanOneBoardSearchOptionPickedException.DEFAULT_MESSAGE}"
+                    TypeError(
+                        f"{method}: Expected AgentTeamSearchContext, got {type(candidate).__name__} instead."
                     )
                 )
             
-            if board_search_context.id is not None:
+            agentTeam_search_context = cast(AgentTeamSearchContext, candidate)
+            if len(agentTeam_search_context.to_dict() == 0):
+                return ValidationResult.failure(
+                    NoAgentTeamSearchOptionSelectedException(
+                        f"{method}: {NoAgentTeamSearchOptionSelectedException.DEFAULT_MESSAGE}"
+                    )
+                )
+            
+            if len(agentTeam_search_context.to_dict()) > 1:
+                return ValidationResult.failure(
+                    MoreThanOneAgentTeamSearchOptionPickedException(
+                        f"{method}: {MoreThanOneAgentTeamSearchOptionPickedException.DEFAULT_MESSAGE}"
+                    )
+                )
+            
+            if agentTeam_search_context.id is not None:
                 return cls.validate_id_search_option(
-                    candidate=board_search_context.id,
-                    id_validator=id_validator
+                    candidate=agentTeam_search_context.id,
+                    identity_service=identity_service
                 )
             
-            if board_search_context.name is not None:
+            if agentTeam_search_context.name is not None:
                 return cls.validate_name_search_option(
-                    name=board_search_context.name,
-                    name_validator=name_validator
+                    name=agentTeam_search_context.name,
+                    identity_service=identity_service
                 )
             
-            if board_search_context.coord is not None:
-                return cls.validate_coord_search_option(
-                    coord=board_search_context.coord,
-                    coord_validator=coord_validator
+            if agentTeam_search_context.color is not None:
+                return cls.validate_color_search_option(
+                    color=agentTeam_search_context.color,
+                    color_validator=color_validator
                 )
         
         except Exception as ex:
             return ValidationResult.failure(
-                InvalidBoardSearchContextException(
-                    f"{method}: {InvalidBoardSearchContextException.DEFAULT_MESSAGE}",
+                InvalidAgentTeamSearchContextException(
+                    f"{method}: {InvalidAgentTeamSearchContextException.DEFAULT_MESSAGE}",
                     ex
                 )
             )
@@ -127,15 +131,16 @@ class BoardSearchContextValidator(Validator):
     def validate_id_search_option(
             cls,
             candidate: Any,
-            id_validator: type[IdValidator] = IdValidator
-    ) -> ValidationResult[BoardSearchContext]:
+            identity_service: type[IdentityService] = IdentityService
+    ) -> ValidationResult[AgentTeamSearchContext]:
         """
         # Action:
         Verify an id_candidate meets application AgentTeamSearchContext safety requirements.
 
         # Parameters:
-            *   candidate (Any):                    Object to verify is an id.
-            *   id_validator (type[IdValidator]):   Checks if candidate complies with safety contract.
+            *   candidate (Any):                            Object to verify is an id.
+            
+            *   identity_service (type[IdentityService]):   Checks if candidate complies with safety contract.
 
         # Returns:
           ValidationResult[AgentTeamSearchContext] containing either:
@@ -143,20 +148,22 @@ class BoardSearchContextValidator(Validator):
                 - On failure:   Exception.
 
         # Raises:
-            *   InvalidBoardSearchContextException
+            *   InvalidAgentTeamSearchContextException
         """
-        method = "BoardSearchContextValidator.validate_id_search_option"
+        method = "AgentTeamSearchContextValidator.validate_id_search_option"
         
         try:
-            id_validation = id_validator.validate(candidate)
+            id_validation = identity_service.validate(candidate)
             if id_validation.is_failure():
                 return ValidationResult.failure(id_validation.exception)
             
-            return ValidationResult.success(payload=BoardSearchContext(id=id_validation.payload))
+            id = cast(int, id_validation.payload)
+            
+            return ValidationResult.success(payload=AgentTeamSearchContext(id=id))
         except Exception as ex:
             return ValidationResult.failure(
-                InvalidBoardSearchContextException(
-                    f"{method}: {InvalidBoardSearchContextException.DEFAULT_MESSAGE}",
+                InvalidAgentTeamSearchContextException(
+                    f"{method}: {InvalidAgentTeamSearchContextException.DEFAULT_MESSAGE}",
                     ex
                 )
             )
@@ -166,15 +173,16 @@ class BoardSearchContextValidator(Validator):
     def validate_name_search_option(
             cls,
             candidate: Any,
-            name_validator: type[NameValidator] = NameValidator
-    ) -> ValidationResult[BoardSearchContext]:
+            identity_service: type[IdentityService] = IdentityService
+    ) -> ValidationResult[AgentTeamSearchContext]:
         """
         # Action:
         Verify a name_candidate meets application AgentTeamSearchContext safety requirements.
 
         # Parameters:
-          * candidate (Any): Object to verify is a name.
-          * name_validator (type[NameValidator]): Checks if candidate complies with safety contract.
+          * candidate (Any):                            Object to verify is a name.
+          
+          * identity_service (type[IdentityService]):   Checks if candidate complies with safety contract.
 
         # Returns:
           ValidationResult[AgentTeamSearchContext] containing either:
@@ -182,39 +190,42 @@ class BoardSearchContextValidator(Validator):
                 - On failure: Exception.
 
         # Raises:
-            * InvalidBoardSearchContextException
+            * InvalidAgentTeamSearchContextException
         """
-        method = "BoardSearchContextValidator.validate_name_search_option"
+        method = "AgentTeamSearchContextValidator.validate_name_search_option"
         
         try:
-            name_validation = name_validator.validate(candidate)
+            name_validation = identity_service.validate(candidate)
             if name_validation.is_failure():
                 return ValidationResult.failure(name_validation.exception)
             
-            return ValidationResult.success(payload=BoardSearchContext(name=name_validation.payload))
+            name = cast(str, name_validation.payload)
+            
+            return ValidationResult.success(payload=AgentTeamSearchContext(name=name))
         
         except Exception as ex:
             return ValidationResult.failure(
-                InvalidBoardSearchContextException(
-                    f"{method}: {InvalidBoardSearchContextException.DEFAULT_MESSAGE}",
+                InvalidAgentTeamSearchContextException(
+                    f"{method}: {InvalidAgentTeamSearchContextException.DEFAULT_MESSAGE}",
                     ex
                 )
             )
     
     @classmethod
     @LoggingLevelRouter.monitor
-    def validate_coord_search_option(
+    def validate_color_search_option(
             cls,
             candidate: Any,
-            coord_validator: type[CoordValidator] = CoordValidator
-    ) -> ValidationResult[BoardSearchContext]:
+            color_validator: type[GameColorValidator] = GameColorValidator
+    ) -> ValidationResult[AgentTeamSearchContext]:
         """
         # Action:
-        Verify a coord_candidate meets application AgentTeamSearchContext safety requirements.
+        Verify a color_candidate meets application AgentTeamSearchContext safety requirements.
 
         # Parameters:
-            *   candidate (Any):                        Object to verify is a coord.
-            *   name_validator (type[CoordValidator]):  Checks if candidate complies with safety contract.
+            *   candidate (Any):                                Object to verify is a color.
+            
+            *   identity_service (type[GameColorValidator]):    Checks if candidate complies with safety contract.
 
         # Returns:
           ValidationResult[AgentTeamSearchContext] containing either:
@@ -222,20 +233,22 @@ class BoardSearchContextValidator(Validator):
                 - On failure:    Exception.
 
         # Raises:
-            *   InvalidBoardSearchContextException
+            *   InvalidAgentTeamSearchContextException
         """
-        method = "BoardSearchContextValidator.validate_coord_search_option"
+        method = "AgentTeamSearchContextValidator.validate_color_search_option"
         
         try:
-            coord_validation = coord_validator.validate(candidate)
-            if coord_validation.is_failure():
-                return ValidationResult.failure(coord_validation.exception)
+            color_validation = color_validator.validate(candidate)
+            if color_validation.is_failure():
+                return ValidationResult.failure(color_validation.exception)
             
-            return ValidationResult.success(payload=BoardSearchContext(coord=coord_validation.payload))
+            color = cast(GameColor, color_validation.payload)
+            
+            return ValidationResult.success(payload=AgentTeamSearchContext(color=color))
         except Exception as ex:
             return ValidationResult.failure(
-                InvalidBoardSearchContextException(
-                    f"{method}: {InvalidBoardSearchContextException.DEFAULT_MESSAGE}",
+                InvalidAgentTeamSearchContextException(
+                    f"{method}: {InvalidAgentTeamSearchContextException.DEFAULT_MESSAGE}",
                     ex
                 )
             )
