37ec03d0976adec5a642998598880b88180cf8d6
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-18 22:14:02 -0600

    Wrote CoordStackExceptions and documented them.

--- src/chess/piece/stack/exception.py
+++ src/chess/piece/stack/exception.py
@@ -1,114 +1,123 @@
-# src/chess/vector/rollback_exception.py
+# src/chess/piece/stack/exception.py
 
 """
-Module: chess.vector.rollback_exception
+Module: chess.piece.stack.exception
 Author: Banji Lawal
 Created: 2025-09-30
 version: 1.0.0
-
-SCOPE:
------
-This module is exclusively for defining all custom **rollback_exception classes** that are specific to the
-creation, validator, and manipulation of `Vector` objects.
-
-**Limitations** It does not contain any logic for raising these exceptions; that responsibility
-`Vector`, `VectorBuilder`, and `VectorValidator`
-
-THEME:
------
-* Granular, targeted error reporting
-* Wrapping exceptions
-
-**Design Concepts**:
-  1. Each consistency and behavior in the `Vector` class has an rollback_exception specific to its possible
-      state, outcome, or behavior.
-
-PURPOSE:
--------
-1. Centralized error dictionary for the `Vector` graph.
-2. Fast debugging using highly granular rollback_exception messages and naming to
-    find the source.
-3. Providing understandable, consistent information about failures originating from
-    the `Vector` graph.
-4. Providing a clear distinction between errors related to `Vector` instances and
-    errors from Python, the Operating System or elsewhere in the `ChessBot` application.
-
-DEPENDENCIES:
-------------
-Requires base rollback_exception classes and constants from the core system:
-From `chess.system`:
-  * Exceptions: `ChessException`, `ValidationException`, `NullException`,
-        `BuildFailedException`.
-
-CONTAINS:
---------
-See the list of exceptions in the `__all__` list following (e.g., `VectorException`,
-`NullVectorException`, `InvalidVectorException`, ).
 """
 
-from chess.exception import ChessException, ValidationException, NullException
+
+from chess.system import ChessException, ValidationException, NullException
+
 
 __all__ = [
-  'CoordStackException',
-
-  'NullCoordStackException',
-  'CoordStackValidationException'
+  "CoordStackException",
+  
+  # ======================# NULL COORD_STACK EXCEPTIONS #======================#
+  "NullCoordStackException",
+  
+  # ======================# COORD_STACK VALIDATION EXCEPTIONS #======================#
+  "InvalidCoordStackException",
+  "CoordStackSizeConflictException",
+  "InconsistentCurrentCoordException",
+  "CorruptedCoordStackException",
+  
+  # ======================# COORD_STACK OPERATION EXCEPTIONS #======================#
+  "PoppingEmptyCoordStackException",
+  "PushingDuplicateCoordException",
+  "PushingNullException",
+  
+  # ======================# COORD_STACK_SERVICE EXCEPTIONS #======================#
+  "CoordStackServiceException",
+  
+  # ======================# NULL COORD_STACK_SERVICE EXCEPTIONS #======================#
+  "NullCoordStackServiceException",
+  
+  # ======================# COORD_STACK VALIDATION EXCEPTIONS #======================#
+  "InvalidCoordStackServiceException",
 ]
 
-from chess.system import InconsistentCollectionException
 
-"""
-Super class for Piece exceptions
-"""
-class CoordStackException(InconsistentCollectionException):
+class CoordStackException(ChessException):
   """
-  Super class for exceptions raised by CoordStack objects
+  Super class for exceptions raised by CoordStack objects. DO NOT USE DIRECTLY. Subclasses
+  give more useful debugging messages.
   """
   ERROR_CODE = "COORD_STACK_ERROR"
-  DEFAULT_MESSAGE = "CoordStack raised an rollback_exception."
-  
-  
-class CoordStakOperationException(CoordStackException):
-  pass
-  
-class CoordStackConsistencyException(CoordStackException, InconsistentCollectionException):
-  pass
+  DEFAULT_MESSAGE = "CoordStack raised an exception."
 
 
-class PopEmptyStackException(CoordStackException):
-  """Raised when trying to pop from empty stack."""
-  ERROR_CODE = "POP_EMPTY_STACK_ERROR"
-  DEFAULT_MESSAGE = "Cannot pop from empty stack"
-
-class DuplicatePushException(CoordStackException):
-  """Raised when trying to push duplicate to stack that doesn'candidate allow duplicates."""
-  ERROR_CODE = "DUPLICATE_PUSH_ERROR"
-  DEFAULT_MESSAGE = "Cannot push duplicate item to stack"
-
-
+# ======================# NULL COORD_STACK EXCEPTIONS #======================#
 class NullCoordStackException(CoordStackException, NullException):
-  """Raised team_name CoordStack is null. A null CoordStack indicates team_name corrupted Piece state"""
+  """Raised if an entity, method, or operation requires CoordStack but gets null instead."""
   ERROR_CODE = "NULL_COORD_STACK_ERROR"
-  DEFAULT_MESSAGE = f"CoordStack is null. This should never happen because Piece must always have team_name CoordStack"
+  DEFAULT_MESSAGE = "CoordStack cannot be null."
 
 
-class CoordStackValidationException(CoordStackException):
-  ERROR_CODE = "COORDINATE_STACK_VALIDATION_ERROR"
-  DEFAULT_MESSAGE = f"CoordinateStack validation failed."
+# ======================# COORD_STACK VALIDATION EXCEPTIONS #======================#
+class InvalidCoordStackException(CoordStackException, ValidationException):
+  """Catchall Exception for CoordStackValidator when a validation candidate fails a sanity check."""
+  ERROR_CODE = "COORD_STACK_VALIDATION_ERROR"
+  DEFAULT_MESSAGE = "CoordStack validation failed."
 
 
+class CoordStackSizeConflictException(CoordStackException, ValidationException):
+  """Raised if coord_stack.size and coord_stack.is_empty() contradict each other."""
+  ERROR_CODE = "COORD_STACK_SIZE_CONFLICT_ERROR"
+  DEFAULT_MESSAGE = "CoordStack.size and CoordStack.is_empty() give contradictory results."
 
 
-
-
-
-
-class PushNullException(CoordStackException):
-  """Raised when trying to push null item to stack."""
-  ERROR_CODE = "PUSH_NULL_ERROR"
-  DEFAULT_MESSAGE = "Cannot push null item to stack"
-
-
-class InconsistentCurrentCoordException(CoordStackException):
+class InconsistentCurrentCoordException(CoordStackException, ValidationException):
+  """Raised if coord_stack.current_coord is not null but the stack is empty or vice versa."""
   ERROR_CODE = "INCONSISTENT_CURRENT_COORD_ERROR"
-  DEFAULT_MESSAGE = "Current coordinate state is inconsistent"
+  DEFAULT_MESSAGE = "The CoordStack.current_coord CoordStack.is_empty() contradict each other."
+
+
+class CorruptedCoordStackException(CoordStackException, ValidationException):
+  """Raised if the list is null. That is CoordStack.items is null"""
+  ERROR_CODE = "CORRUPTED_COORD_STACK_ERROR"
+  DEFAULT_MESSAGE = "CoordStack.items is null. There is inconsistent data in the system."
+
+
+# ======================# COORD_STACK OPERATION EXCEPTIONS #======================#
+class PoppingEmptyCoordStackException(CoordStackException):
+  """Raised when trying to pop from an empty CoordStack."""
+  ERROR_CODE = "POPPING_EMPTY_STACK_ERROR"
+  DEFAULT_MESSAGE = "Cannot pop from an empty CoordStack."
+
+
+class PushingDuplicateCoordException(CoordStackException):
+  """Raised when trying to push the same coord onto the stack."""
+  ERROR_CODE = "PUSHING_DUPLICATE_COORD_ERROR"
+  DEFAULT_MESSAGE = "Cannot push duplicate coord onto the stack. All Coords must be unique."
+
+
+class PushingNullException(CoordStackException):
+  """Raised when trying to push null item to stack."""
+  ERROR_CODE = "PUSHING_NULL_ERROR"
+  DEFAULT_MESSAGE = "Cannot push null item onto a CoordStack."
+
+
+# ======================# COORD_STACK_SERVICE EXCEPTIONS #======================#
+class CoordStackServiceException(ChessException):
+  """
+  Super class for exceptions raised by CoordStackService objects. DO NOT USE DIRECTLY. Subclasses
+  give more useful debugging messages.
+  """
+  ERROR_CODE = "COORD_STACK_SERVICE_ERROR"
+  DEFAULT_MESSAGE = "CoordStackService raised an exception."
+
+
+# ======================# NULL COORD_STACK_SERVICE EXCEPTIONS #======================#
+class NullCoordStackServiceException(CoordStackServiceException, NullException):
+  """Raised if an entity, method, or operation requires CoordStackService but gets null instead."""
+  ERROR_CODE = "NULL_COORD_STACK_SERVICE_ERROR"
+  DEFAULT_MESSAGE = "CoordStackService cannot be null."
+
+
+# ======================# COORD_STACK VALIDATION EXCEPTIONS #======================#
+class InvalidCoordStackServiceException(CoordStackServiceException, ValidationException):
+  """Catchall Exception for CoordStackServiceValidator when a validation candidate fails a sanity check."""
+  ERROR_CODE = "COORD_STACK_SERVICE_VALIDATION_ERROR"
+  DEFAULT_MESSAGE = "CoordStackService validation failed."
--- src/chess/piece/stack/validator.py
+++ src/chess/piece/stack/validator.py
@@ -7,115 +7,112 @@ Created: 2025-09-30
 version: 1.0.0
 """
 
-
 from typing import Any, cast
 
-from chess.piece import CoordStack, CoordStackValidationException, NullCoordStackException
 from chess.system import LoggingLevelRouter, Validator, ValidationResult
+from chess.piece import (
+    CoordStack, CoordStackSizeConflictException, CorruptedCoordStackException,
+    InconsistentCurrentCoordException, InvalidCoordStackException, NullCoordStackException
+)
 
 
 class CoordStackValidator(Validator[CoordStack]):
-  """
-  # ROLE: Validation
-
-  # RESPONSIBILITIES:
-  Verifies a candidate is an instance of Team, that meets integrity requirements, before
-  the candidate is used.
-
-  # PROVIDES:
-  ValidationResult[Team] containing either:
-      - On success: Team in the payload.
-      - On failure: Exception.
-
-  # ATTRIBUTES:
-  No attributes
-  """
-
-  @classmethod
-  @LoggingLevelRouter.monitor
-  def validate(cls, candidate: Any) -> ValidationResult[CoordStack]:
     """
-    # ACTION:
-    1.  Check candidate is not null.
-    2.  Check if candidate is a Team. If so cast it.
-    3.  Verify the CoordStack.items is not null.
-    4.  Verify Coordstack.size and CoordStack.is_empty() do not conflict.
-    5.  Verify CoordStack.current_coord is null if CoordStack.is_empty()
-        do not conflict.
-    6.  If any check fails, return the exception inside a ValidationResult.
-    7.  If all pass return the Team object in a ValidationResult
-
-    # PARAMETERS:
-        *   candidate (Any):  Object to validate as a Team object.
-
-    # Returns:
-    ValidationResult[Team] containing either:
-        - On success:   Team in the payload.
-        - On failure:   Exception.
-
-    # RAISES:
-        *   TypeError
-        *   NullTeamException
-        *   CorruptedCoordStackException
-        *   InconsistentCurrentCoordException
-        *   TeamNotRegisteredWithAgentException
-        *   InvalidCoordStackException
+    # ROLE: Validation
+  
+    # RESPONSIBILITIES:
+    Verifies a candidate is an instance of CoordStack, that meets integrity requirements, before
+    the candidate is used.
+  
+    # PROVIDES:
+    ValidationResult[CoordStack] containing either:
+        - On success: CoordStack in the payload.
+        - On failure: Exception.
+  
+    # ATTRIBUTES:
+    No attributes
     """
-    method = "CoordinateStackValidator.validate"
-
-    try:
-      if candidate is None:
-        return ValidationResult.failure(
-          NullCoordStackException(f"{method}: {NullCoordStackException.DEFAULT_MESSAGE}")
-        )
-
-      if not isinstance(candidate, CoordStack):
-        return ValidationResult.failure(
-          TypeError(
-            f"{method} Expected CoordStack, got {type(candidate).__name__} instead."
-          )
-        )
-
-      coord_stack = cast(CoordStack, candidate)
-      
-      if coord_stack.items is None:
-        return ValidationResult.failure(
-          CorruptedCoordStackException(
-            f"{method} {CorruptedCoordStackException.DEFAULT_MESSAGE}"
-          )
-        )
-      
-      if (
-              coord_stack.size > 0 and coord_stack.is_empty() or
-              coord_stack.size == 0 and not coord_stack.is_empty()
-      ):
-        return ValidationResult.failure(
-          CoordStackSizeConflictException(
-            f"{method}: {CoordStackSizeConflictException.DEFAULT_MESSAGE}"
-          )
-        )
-      
-      if (
-              coord_stack.is_empty() and coord_stack.current_coord is not None or
-              not coord_stack.is_empty() and coord_stack.current_team is None
-      ):
-        return ValidationResult.failure(
-          InconsistentCurrentCoordException(
-            f"{method}: {InconsistentCurrentCoordException.DEFAULT_MESSAGE}"
-          )
-        )
-      
-      return ValidationResult.success(payload=coord_stack)
-
-    except (
-        TypeError,
-        NullCoordStackException,
-        StackSizeConflictException,
-        InconsistentCurrentCoordException
-    ) as ex:
-      return ValidationResult.failure(
-        InvalidCooordStackException(
-          f"{method}: {InvalidCoordStackException.DEFAULT_MESSAGE}",
-          ex
-        )
-      )
+    
+    @classmethod
+    @LoggingLevelRouter.monitor
+    def validate(cls, candidate: Any) -> ValidationResult[CoordStack]:
+        """
+        # ACTION:
+        1.  Check candidate is not null.
+        2.  Check if candidate is a CoordStack. If so cast it.
+        3.  Verify the CoordStack.items is not null.
+        4.  Verify Coordstack.size and CoordStack.is_empty() do not conflict.
+        5.  Verify CoordStack.current_coord is null if CoordStack.is_empty()
+            do not conflict.
+        6.  If any check fails, return the exception inside a ValidationResult.
+        7.  If all pass return the CoordStack object in a ValidationResult
+    
+        # PARAMETERS:
+            *   candidate (Any):  Object to validate as a CoordStack object.
+    
+        # Returns:
+        ValidationResult[CoordStack] containing either:
+            - On success:   CoordStack in the payload.
+            - On failure:   Exception.
+    
+        # RAISES:
+            *   TypeError
+            *   NullCoordStackException
+            *   CorruptedCoordStackException
+            *   InconsistentCurrentCoordException
+            *   CoordStackNotRegisteredWithAgentException
+            *   InvalidCoordStackException
+        """
+        method = "CoordinateStackValidator.validate"
+        
+        try:
+            if candidate is None:
+                return ValidationResult.failure(
+                    NullCoordStackException(f"{method}: {NullCoordStackException.DEFAULT_MESSAGE}")
+                )
+            
+            if not isinstance(candidate, CoordStack):
+                return ValidationResult.failure(
+                    TypeError(
+                        f"{method} Expected CoordStack, got {type(candidate).__name__} instead."
+                    )
+                )
+            
+            coord_stack = cast(CoordStack, candidate)
+            
+            if coord_stack.items is None:
+                return ValidationResult.failure(
+                    CorruptedCoordStackException(
+                        f"{method} {CorruptedCoordStackException.DEFAULT_MESSAGE}"
+                    )
+                )
+            
+            if (
+                    coord_stack.size > 0 and coord_stack.is_empty() or
+                    coord_stack.size == 0 and not coord_stack.is_empty()
+            ):
+                return ValidationResult.failure(
+                    CoordStackSizeConflictException(
+                        f"{method}: {CoordStackSizeConflictException.DEFAULT_MESSAGE}"
+                    )
+                )
+            
+            if (
+                    coord_stack.is_empty() and coord_stack.current_coord is not None or
+                    not coord_stack.is_empty() and coord_stack.current_coordStack is None
+            ):
+                return ValidationResult.failure(
+                    InconsistentCurrentCoordException(
+                        f"{method}: {InconsistentCurrentCoordException.DEFAULT_MESSAGE}"
+                    )
+                )
+            
+            return ValidationResult.success(payload=coord_stack)
+        
+        except Exception as ex:
+            return ValidationResult.failure(
+                InvalidCoordStackException(
+                    f"{method}: {InvalidCoordStackException.DEFAULT_MESSAGE}",
+                    ex
+                )
+            )
