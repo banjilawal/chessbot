e6de3483385c283425f257119d9e678e059a45e7
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-21 18:57:33 -0600

    1) Added constructor to chess.system.build.Builder.2) Changed validate from class method to instance method with no params.

--- src/chess/system/build/builder.py
+++ src/chess/system/build/builder.py
@@ -12,47 +12,49 @@ from typing import Generic, TypeVar
 
 from chess.system import BuildResult, LoggingLevelRouter
 
-T = TypeVar("V")
+T = TypeVar("T")
+
 
 class Builder(ABC, Generic[T]):
-  """
-  # ROLE: Builder, Data Integrity and Reliability Guarantor.
-
-  # RESPONSIBILITIES:
-  1.  Produce V instances whose integrity is always guaranteed.
-  1.  Uses Validator instances to verify builder resources do not break safety contracts.
-  2.  Wraps ValidatorExceptions inside BuildResult before returning to caller.
-  3.  Performs any additional checks not covered by Validators to assure safety and integrity of new objects.
-
-
-  # PROVIDES:
-  BuildResult[V] containing either:
-      - On success: V in the payload.
-      - On failure: Exception.
-      
-  # ATTRIBUTES:
-  None
-  """
-
-  @classmethod
-  @abstractmethod
-  @LoggingLevelRouter.monitor
-  def build(cls, *args, **kwargs) -> BuildResult[T]:
     """
-    # ACTION:
-    1. Run integrity checks on each parameter required for constructing V.
-    2. If any check fails it raises an exception that is returned inside a BuildResult.
-    3. When all checks pass, construct V then return it inside a BuildResult.
-
-    # PARAMETERS:
-        * args: Parameters for constructing V.
-
-    # Returns:
-    BuildResult[V] containing either:
-        - On success: V in the payload.
+    # ROLE: Builder, Data Integrity and Reliability Guarantor.
+  
+    # RESPONSIBILITIES:
+    1.  Produce V instances whose integrity is always guaranteed.
+    1.  Uses Validator instances to verify builder resources do not break safety contracts.
+    2.  Wraps ValidatorExceptions inside BuildResult before returning to caller.
+    3.  Performs any additional checks not covered by Validators to assure safety and integrity of new objects.
+  
+  
+    # PROVIDES:
+    BuildResult[T] containing either:
+        - On success: T in the payload.
         - On failure: Exception.
-
-    # RAISES:
-      * BuildFailedException
+        
+    # ATTRIBUTES:
     """
-    pass
+    
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    @abstractmethod
+    @LoggingLevelRouter.monitor
+    def build(self) -> BuildResult[T]:
+        """
+        # ACTION:
+        1. Run integrity checks on each parameter required for constructing V.
+        2. If any check fails it raises an exception that is returned inside a BuildResult.
+        3. When all checks pass, construct V then return it inside a BuildResult.
+        
+        # PARAMETERS:
+            * args: Parameters for constructing V.
+        
+        # Returns:
+        BuildResult[V] containing either:
+            - On success: V in the payload.
+            - On failure: Exception.
+        
+        # RAISES:
+          * BuildFailedException
+        """
+        pass
--- src/chess/system/result/result.py
+++ src/chess/system/result/result.py
@@ -8,68 +8,60 @@ Updated: 2025-10-10
 version: 1.0.0
 """
 
-
 from typing import Optional, TypeVar, Generic
 from chess.system import Result
 
 T = TypeVar("T")
-V = TypeVar("V")
 
 
 class Result(Generic[T]):
-  """
-  # ROLE: Message passing, Data Transfer Object
-
-  # RESPONSIBILITIES:
-  1. Carry the outcome of a service access or service generation operation to the caller.
-  2. Transporting errors from the service source to the requester for handling that preserves
-      reliability and availability.
-
-  # PROVIDES:
-  1.
-
-  # Attributes:
-    _payload (Optional[V]): Data from the accessor or service generator if their rollback
-      were successful.
-    _exception (Optional[Exception]): The error raised if the operation called failed.
-  """
-  _payload: Optional[T]
-  _exception: Optional[Exception]
-
-
-  def __init__(self, payload: Optional[T] = None, exception: Optional[Exception] = None):
-    self._payload = payload
-    self._exception = exception
-
-  @property
-  def payload(self) -> Optional[T]:
-    return self._payload
-
-  @property
-  def exception(self) -> Optional[Exception]:
-    return self._exception
-
-  def is_success(self) -> bool:
-    return self._exception is None and self._payload is not None
-
-  def is_failure(self) -> bool:
-    return self._exception is not None
-
-  def is_empty(self) -> bool:
-    return self._payload is None and self._exception is None
-
-  @classmethod
-  def success(cls, payload: T) -> Result[V]:
-    return cls(payload=payload)
-
-  @classmethod
-  def failure(cls, exception: Exception) -> Result[V]:
-    return cls(exception=exception)
-
-  @classmethod
-  def empty(cls) -> Result[None]:
-    return cls()
-
-
-
-
+    """
+    # ROLE: Message passing, Data Transfer Object
+  
+    # RESPONSIBILITIES:
+    1. Carry the outcome of a service access or service generation operation to the caller.
+    2. Transporting errors from the service source to the requester for handling that preserves
+        reliability and availability.
+  
+    # PROVIDES:
+    1.
+  
+    # Attributes:
+        *   payload (T)
+        *   exception (Exception)
+    """
+    _payload: Optional[T]
+    _exception: Optional[Exception]
+    
+    def __init__(self, payload: Optional[T] = None, exception: Optional[Exception] = None):
+        self._payload = payload
+        self._exception = exception
+    
+    @property
+    def payload(self) -> Optional[T]:
+        return self._payload
+    
+    @property
+    def exception(self) -> Optional[Exception]:
+        return self._exception
+    
+    def is_success(self) -> bool:
+        return self._exception is None and self._payload is not None
+    
+    def is_failure(self) -> bool:
+        return self._exception is not None
+    
+    def is_empty(self) -> bool:
+        return self._payload is None and self._exception is None
+    
+    @classmethod
+    def success(cls, payload: T) -> Result[T]:
+        return cls(payload=payload)
+    
+    @classmethod
+    def failure(cls, exception: Exception) -> Result[T]:
+        return cls(exception=exception)
+    
+    @classmethod
+    def empty(cls) -> Result[None]:
+        return cls()
