668429c517329fbb5afd480f7b5205784a2932c9
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-27 00:02:53 -0600

    Cleaned up AgentBuilder and renamed it AgentFactory.

--- src/chess/agent/factory/factory.py
+++ src/chess/agent/factory/factory.py
@@ -15,126 +15,7 @@ from chess.agent import Agent, AgentBuildFailedException, AgentVariety, AgentVal
 from chess.system import Builder, BuildResult, IdentityService, LoggingLevelRouter, ValidationResult, id_emitter
 
 
-class AgentBuilder(Builder[Agent]):
-    """
-    # ROLE: Builder, Data Integrity Guarantor
-
-    # RESPONSIBILITIES:
-    Produce Agent instances whose integrity is always guaranteed. If any attributes do not pass
-    their integrity checks, send an exception instead of an unsafe Agent.
-
-    # PROVIDES:
-    BuildResult[Agent] containing either:
-        - On success: Agent in the payload.
-        - On failure: Exception.
-
-    # ATTRIBUTES:
-    None
-    """
-    
-    @classmethod
-    def build(
-            cls,
-            id: int,
-            name: str,
-            variety: AgentVariety,
-            validator: AgentValidator = AgentValidator(),
-            engine_service: Optional[EngineService] = None,
-            identity_service: IdentityService = IdentityService(),
-            team_stack: UniqueTeamDataService = UniqueTeamDataService(),
-    ) -> BuildResult[Agent]:
-        """
-        # ACTION:
-        1.  Call _validate_build_params. to verify inputs are safe.
-        2.  If the _validate params returns failure include the failure in a BuildResult.
-        3.  If the engine is not validation call build_machine_agent. Otherwise, call build_human_agent.
-
-        # PARAMETERS:
-            *   id (int)
-            *   name (str)
-            *   agent_category (AgentCategory)
-            *   identity_service (IdentityService)
-            *   team_stack (UniqueTeamDataService)
-            *   engine_service (Optional[EngineService])
-
-        # Returns:
-        BuildResult[Agent] containing either:
-            - On success: Agent in the payload.
-            - On failure: Exception.
-
-        # Raises:
-            *   AgentBuildFailedException
-        """
-        method = "AgentBuilder.builder"
-        
-        try:
-            params_validation = cls._validate_params(
-                id=id,
-                name=name,
-                variety=variety,
-                identity_service=identity_service,
-                team_stack=team_stack,
-                engine_service=engine_service,
-            )
-            
-            if engine_service is not None:
-                return cls._build_machine_agent(
-                    id=id,
-                    name=name,
-                    identity_service=identity_service,
-                    team_stack_service=team_stack_service,
-                    engine_service=engine_service,
-                )
-            
-            return cls.build_human_agent(
-                id=id,
-                name=name,
-                identity_service=identity_service,
-                team_stack_service=team_stack_service,
-            )
-        
-        except Exception as ex:
-            return BuildResult.failure(
-                AgentBuildFailedException(
-                    f"{method}: {AgentBuildFailed.DEFAULT_MESSAGE}",
-                    ex
-                )
-            )
-    
-    @classmethod
-    @LoggingLevelRouter.monitor
-    def _build_machine_agent(
-            cls,
-            id: int,
-            name: str,
-            identity_service: IdentityService = IdentityService(),
-            team_stack_service: TeamStackService = TeamStackService(),
-            engine_service: EngineService = EngineService(),
-    ) -> BuildResult[MachinePlayer]:
-        try:
-            param_validation = cls._validate_params(id=id, name=name, identity_service=identity_service)
-            if param_validation.is_failure():
-                return BuildResult.failure(param_validation.exception)
-            
-            return BuildResult.success(
-                MachinePlayer(
-                    id=id,
-                    name=name,
-                    team_stack_service=team_stack_service,
-                    engine_service=engine_service
-                )
-            )
-        except Exception as ex:
-            return BuildResult.failure(
-                AgentBuildFailedException(
-                    "f{method}: {AgentBuildFailed.DEFAULT_MESSAGE}",
-                    ex
-                )
-            )
-    
-    from chess.system import Builder, BuildResult, IdentityService, LoggingLevelRouter, ValidationResult
-    
-    class AgentBuilder(Builder[Agent]):
+class AgentFactory(Builder[Agent]):
         """
         # ROLE: Builder, Data Integrity Guarantor
 
@@ -183,7 +64,7 @@ class AgentBuilder(Builder[Agent]):
             # Raises:
                 *   AgentBuildFailedException
             """
-            method = "AgentBuilder.builder"
+            method = "AgentBuilder.build"
             
             try:
                 variety_validation = agent_validator.certify_agent_variety(candidate=variety)
@@ -298,23 +179,3 @@ class AgentBuilder(Builder[Agent]):
                         )
                     )
                 )
-
-#
-#
-# def main():
-#   build_result = AgentBuilder.builder(commander_id=id_emitter.person_id, visitor_name=RandomName.person())
-#   if build_result.is_success():
-#     competitor = build_result.payload
-#     print(f"Successfully built competitor: {competitor}")
-#   else:
-#     print(f"Failed to builder competitor: {build_result.err}")
-#
-#   build_result = AgentBuilder.builder(-1, 4)
-#   if build_result.is_success():
-#     competitor = build_result.payload
-#     print(f"Successfully built competitor: {competitor}")
-#   else:
-#     print(f"Failed to builder competitor: {build_result.err}")
-#
-# if __name__ == "__main__":
-#   main()
