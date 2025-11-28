44eb39bd4dd554c3b7a54831b2b130d3b767d7a1
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-27 22:25:10 -0600

    Documented chess.team.service.__init__ and chess.team.data.__init__.

--- src/chess/team/service/data/__init__.py
+++ src/chess/team/service/data/__init__.py
@@ -7,8 +7,11 @@ Created: 2025-11-24
 version: 1.0.0
 """
 
+# =========== TEAM.SERVICE.DATA PACKAGE CONTENTS ===========#
 
-from .exception import *
+# Packages
 from .unique import *
 
+# Modules
+from .exception import *
 from .service import  TeamDataService
