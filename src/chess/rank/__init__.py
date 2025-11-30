7df23ce13cf310c64dc2baffc85dfe5df7707e48
Banji Lawal <banji.k.lawal@gmail.com>
2025-11-29 18:56:44 -0600

    Moved king, queen, rook, pawn, knight and bishop from chess.rank  into their own subpackages.

--- src/chess/rank/__init__.py
+++ src/chess/rank/__init__.py
@@ -7,17 +7,23 @@ Created: 2025-09-08
 version: 1.0.0
 """
 
+# =========== RANK.SPEC PACKAGE CONTENTS ===========#
+
+# Packages
+from .exception import *
+from .bishop import *
+from .king import *
+from .knight import *
+from .pawn import *
+from .queen import *
+from .spec import *
+from .rook import *
+
+# Modules
 from .exception import *
 from .factory import *
 from .search import *
 from .service import *
 from .spec import *
 from .validator import *
-
 from .rank import Rank
-from .king import King
-from .rook import Rook
-from .pawn import Pawn
-from .queen import Queen
-from .knight import Knight
-from .bishop import Bishop
--- src/chess/rank/spec/exception/exception.py
+++ src/chess/rank/spec/exception/exception.py
@@ -44,34 +44,3 @@ class NullRankSpecException(RankSpecException, NullException):
     """Raised if an entity, method, or operation requires a RankSpec but gets null."""
     ERROR_CODE = "NULL_RANK_SPEC_ERROR"
     DEFAULT_MESSAGE = "RankSpec cannot be null."
-
-#
-# # ======================# RANK_SPEC BOUNDS EXCEPTIONS #======================#
-# class RankIdBoundsException(RankSpecException, BoundsException):
-#     """Raised if a number is not in a RankSpec ids."""
-#     ERROR_CODE = "RANK_ID_BOUNDS_ERROR"
-#     DEFAULT_MESSAGE = "Number is outside the range of Rank.ids allowed in RankSpec."
-#
-#
-# class RankNameBoundsException(RankSpecException, BoundsException):
-#     """Raised if a value is not in a RankSpec names."""
-#     ERROR_CODE = "RANK_NAME_BOUNDS_ERROR"
-#     DEFAULT_MESSAGE = "String is outside the range of Rank.names allowed in RankSpec."
-#
-#
-# class RankDesignationBoundsException(RankSpecException, BoundsException):
-#     """Raised if a string is not in a RankSpec designations."""
-#     ERROR_CODE = "RANK_DESIGNATION_BOUNDS_ERROR"
-#     DEFAULT_MESSAGE = "String is outside the range of Rank.designations allowed in RankSpec."
-#
-#
-# class RankRansomBoundsException(RankSpecException, BoundsException):
-#     """Raised if a number is not in a RankSpec ransoms."""
-#     ERROR_CODE = "RANK_RANSOM_BOUNDS_ERROR"
-#     DEFAULT_MESSAGE = "Number is outside the range of Rank.ransoms allowed in RankSpec."
-#
-#
-# class TeamQuotaBoundsException(RankSpecException, BoundsException):
-#     """Raised if a value is not in a RankSpec team_quotas."""
-#     ERROR_CODE = "RANK_QUOTA_BOUNDS_ERROR"
-#     DEFAULT_MESSAGE = "Number is outside the range of Rank.team_quotas allowed in RankSpec."
