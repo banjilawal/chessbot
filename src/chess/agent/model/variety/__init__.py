576ea2629eab630cda1e8116bf45955c555fbf42
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-07 00:59:22 -0600

    Documentation for chess.agent.model.variety.__init__.py

--- src/chess/agent/model/variety/__init__.py
+++ src/chess/agent/model/variety/__init__.py
@@ -7,5 +7,11 @@ Created: 2025-09-16
 version: 1.0.0
 """
 
+# =========== AGENT.MODEL.VARIETY PACKAGE CONTENTS ===========#
+
+# Packages
+None
+
+# Modules
 from .exception import *
 from .variety import AgentVariety
