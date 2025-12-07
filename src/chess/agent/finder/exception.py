56785c2801ef8f24656fbef8246b9d9d86927b37
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-07 02:38:45 -0600

    Wrote role-responsibility pydoc for AgentFinderException.

--- src/chess/agent/finder/exception.py
+++ src/chess/agent/finder/exception.py
@@ -1,7 +1,7 @@
-# src/chess/agent/searcher/exception
+# src/chess/agent/finder/exception
 
 """
-Module: chess.agent.searcher.exception
+Module: chess.agent.finder.exception
 Author: Banji Lawal
 Created: 2025-11-17
 version: 1.0.0
@@ -18,8 +18,23 @@ __all__ = [
 # ======================# AGENT_SEARCH EXCEPTIONS #======================#
 class AgentFinderException(FinderException):
   """
-  Super class of exceptions raised by AgentFinder objects. Do not use directly. Subclasses give
-  precise, fined-grained, debugging info.
+  # ROLE: Exception Wrapper, Catchall Exception
+
+  # RESPONSIBILITIES:
+  1.  Parent of exceptions raised when: data in an AgentFinder's fields or unanticipated conditions
+      halt the normal flow of AgentFinder operations.
+  2.  Wraps unhandled exceptions that hit the try-finally block of an AgentFactory method.
+
+  # PARENT
+      *   FinderException
+
+  # PROVIDES:
+  BuildResult[Agent] containing either:
+          - On success: Agent in the payload.
+          - On failure: Exception.
+
+  # ATTRIBUTES:
+  None
   """
-  ERROR_CODE = "AGENT_SEARCH_ERROR"
+  ERROR_CODE = "AGENT_FINDER_ERROR"
   DEFAULT_MESSAGE = "AgentFinder raised an exception."
