2324f0a0bc4c37b436c9f82eca0b3811ef8350e1
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-07 23:16:31 -0600

    1) Moved AgentRegistrationExceptions from a unified, monolithic module into separate ones.

--- src/chess/agent/validator/exception/registration/__init__.py
+++ src/chess/agent/validator/exception/registration/__init__.py
@@ -13,4 +13,5 @@ version: 1.0.0
 None
 
 # Modules
-from .exception import *
+from .base import AgentRegistrationException
+from .game import AgentNotRegisteredWithGameException
--- src/chess/agent/validator/exception/registration/game.py
+++ src/chess/agent/validator/exception/registration/game.py
@@ -1,45 +1,21 @@
-# src/chess/agent/validator/exception/registration/exception.py
+# src/chess/agent/validator/exception/registration/game.py
 
 """
-Module: chess.agent.validator.exception.registration.exception
+Module: chess.agent.validator.exception.registration.game
 Author: Banji Lawal
 Created: 2025-09-16
 version: 1.0.0
 """
 
-from chess.agent import InvalidAgentException
-from chess.system import RegistrationException
+from chess.agent import AgentRegistrationException
+
 
 __all__ = [
     # ======================# AGENT REGISTRATION EXCEPTION SUPER CLASS #======================#
-    "AgentRegistrationException"
+    "AgentNotRegisteredWithGameException",
 ]
 
 
-# ======================# AGENT_REGISTRATION EXCEPTION SUPER CLASS #======================#
-class AgentRegistrationException(InvalidAgentException, RegistrationException):
-    """
-    # ROLE: Exception Wrapper, Catchall Exception
-
-    # RESPONSIBILITIES:
-    1.  Catchall Exception for when an Agent has set its owner correctly but the owner does not
-        have the agent in its collection.
-
-    # PARENT:
-        *   InvalidAgentException
-        *   RegistrationException
-
-    # PROVIDES:
-    AgentRegistrationException
-
-    # LOCAL ATTRIBUTES:
-    None
-
-    # INHERITED ATTRIBUTES:
-    None
-    """
-    ERROR_CODE = "AGENT_REGISTRATION_ERROR"
-    DEFAULT_MESSAGE = "Agent not registered with parent."
 
 
 # ======================# AGENT_NOT_REGISTERED_WITH_GAME EXCEPTION #======================#
