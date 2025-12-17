beb5980f396f9842c85c627b9a40d0346c2998b2
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-17 13:24:45 -0600

    Wrote Metadata pydocs.

--- src/chess/system/metadata/metadata.py
+++ src/chess/system/metadata/metadata.py
@@ -13,7 +13,25 @@ from typing import List
 
 
 class Metadata(ABC):
-    
+    """
+    # ROLE: Configuration Table, Metadata
+
+    # RESPONSIBILITIES:
+    1.  Provides a set of static values assigned to strategy-using subclasses for inherited or common attributes.
+
+    # PARENT:
+    None
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    # INHERITED ATTRIBUTES:
+    None
+    """
     @property
     def allowed_names(self) -> List[str]:
+        """List of names associated with each subclass according to the metadata set."""
         pass
