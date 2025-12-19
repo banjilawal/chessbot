04c545887cd3219c204e9a4692a1364672df9693
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-18 19:32:34 -0600

    Wrote pydoc for NullCoordException.

--- src/chess/coord/validator/exception/null.py
+++ src/chess/coord/validator/exception/null.py
@@ -6,3 +6,37 @@ Author: Banji Lawal
 Created: 2025-11-19
 version: 1.0.0
 """
+
+from chess.system import NullException
+from chess.coord import InvalidCoordException
+
+__all__ = [
+    # ======================# NULL COORD EXCEPTION #======================#
+    "NullCoordException",
+]
+
+
+# ======================# NULL COORD EXCEPTION #======================#
+class NullCoordException(InvalidCoordException, NullException):
+    """
+    # ROLE: Error Tracing, Debugging
+
+    # RESPONSIBILITIES:
+    1.  Raised if a Coord validation candidate is null.
+    2.  Raised if an entity, method or operation requires a Coord but receives null instead.
+
+    # PARENT:
+        *   InvalidCoordException
+        *   NullCoordException
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    # INHERITED ATTRIBUTES:
+    None
+    """
+    ERROR_CODE = "NULL_COORD___ERROR"
+    DEFAULT_MESSAGE = "Coord cannot be null."
