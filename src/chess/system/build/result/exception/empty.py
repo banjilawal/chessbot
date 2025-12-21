d5b751e23db5eeeec9ea79c0db6c74b4646f9431
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-21 13:54:36 -0600

    Documented chess.system.build.result.exception.EmptyBuildResultException.

--- src/chess/system/build/result/exception/empty.py
+++ src/chess/system/build/result/exception/empty.py
@@ -7,10 +7,35 @@ Created: 2025-10-03
 version: 1.0.0
 """
 
-from chess.system import ResultException
+from chess.system import BuildException, ResultException
 
 
+__all__ = [
+    # ======================# EMPTY_BUILD_RESULT EXCEPTION #======================#
+    "EmptyBuildResultException",
+]
+
+
+# ======================# EMPTY_BUILD_RESULT EXCEPTION #======================#
 class EmptyBuildResultException(BuildException, ResultException):
-    """BuildResult must contain either a payload or an exception"""
+    """
+    # ROLE: Messanger  Data Transport Object, Error Transport Object.
+
+    # RESPONSIBILITIES:
+    1.  Indicate that a Build result contains neither a payload nor an exception.
+
+    # PARENT:
+        *   BuildException
+        *   ResultException
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    INHERITED ATTRIBUTES:
+    None
+    """
     ERROR_CODE = "EMPTY_BUILD_RESULT_ERROR"
     DEFAULT_MESSAGE = "BuildResult must contain either a payload or an exception. It cannot be empty."
