40d1c2b0590de198581776d8db1c58c3a8e85a9f
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-20 18:39:43 -0600

    Updated ZeroAgentContextFlagsException's pydocs.

--- src/chess/agent/context/validator/exception/flag/zero.py
+++ src/chess/agent/context/validator/exception/flag/zero.py
@@ -8,7 +8,7 @@ version: 1.0.0
 """
 
 
-from chess.system import BoundsException
+from chess.system import ContextFlagCountException
 from chess.agent import InvalidAgentContextException
 
 __all__ = [
@@ -18,16 +18,16 @@ __all__ = [
 
 
 # ========================= ZERO_AGENT_CONTEXT_FLAGS EXCEPTION =========================#
-class ZeroAgentContextFlagsException(InvalidAgentContextException, BoundsException):
+class ZeroAgentContextFlagsException(InvalidAgentContextException, ContextFlagCountException):
     """
     # ROLE: Error Tracing, Debugging
 
     # RESPONSIBILITIES:
-    1.  Indicates no AgentContext flag was enabled. One and only one Agent attribute-value tuple is required for
+    1.  Indicates no AgentContext flag was enabled. One and only one Agent attribute-value-tuple is required for
         a search.
 
     # PARENT:
-        *   BoundsException
+        *   ContextFlagCountException
         *   InvalidAgentContextException
 
     # PROVIDES:
@@ -40,4 +40,7 @@ class ZeroAgentContextFlagsException(InvalidAgentContextException, BoundsExcepti
     None
     """
     ERROR_CODE = "ZERO_AGENT_CONTEXT_FLAGS_ERROR"
-    DEFAULT_MESSAGE = "Zero AgentContext flags were set. One and only one context flag must be enabled,"
+    DEFAULT_MESSAGE = (
+        "Zero AgentContext flags were set. Cannot search for Agents if one-and_oly-one "
+        "context flag is enabled."
+    )
