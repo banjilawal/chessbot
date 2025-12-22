f732bc56e6ad27d77f8733e67c919d7d99b56d16
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-22 09:21:27 -0600

    Cleaned up AgentContextServiceException documentation.

--- src/chess/agent/context/service/exception.py
+++ src/chess/agent/context/service/exception.py
@@ -7,6 +7,7 @@ Created: 2025-09-16
 version: 1.0.0
 """
 
+from chess.agent import AgentContext
 from chess.system import ServiceException
 
 __all__ = [
@@ -16,17 +17,16 @@ __all__ = [
 
 
 #======================# AGENT_CONTEXT_SERVICE EXCEPTION #======================#
-class AgentContextServiceException(ServiceException):
+class AgentContextServiceException(AgentContext, ServiceException):
     """
     # ROLE: Exception Wrapper, Catchall Exception
     
     # RESPONSIBILITIES:
-    1.  Parent of exception raised when an AgentContextService's normal operations are halted
-        by an error condition.
-    2.  Raised when no specific exception exists for the error interrupting AgentContextService's
-        processes from their normal flows.
+    1.  Indicate that an AgentContextService encountered an error which prevented the service from completing a task.
+    2.  Wrap an exception that hits the try-finally block of an AgentContextService method.
         
     # PARENT:
+        *   AgentException
         *   ServiceException
 
     # PROVIDES:
