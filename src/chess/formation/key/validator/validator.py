6c022655edae17815a11adaf456d06f6d599ae05
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-24 15:55:14 -0600

    Wrote FormatioSuperKeyValidator.

--- src/chess/formation/key/validator/validator.py
+++ src/chess/formation/key/validator/validator.py
@@ -1,27 +1,25 @@
-# src/chess/formation/validator/validator.py
+# src/chess/formation/key/validator/validator.py
 
 """
-Module: chess.formation.validator
+Module: chess.formation.key.validator.validator
 Author: Banji Lawal
 Created: 2025-10-09
 version: 1.0.0
 """
+# 
+
 
 from typing import Any, cast
 
-from chess.system import GameColorValidator, IdentityService, LoggingLevelRouter, ValidationResult, Validator
-from chess.formation import (
-    InvalidOrderContextException, NoOrderContextFlagException, NullOrderContextException, OrderContext,
-    ExcessiveOrderContextFlagsException
-)
+from chess.system import Validator
 
 
-class OrderContextValidator(Validator[OrderContext]):
+class FormationSuperKeyValidator(Validator[FormationSuperKey]):
     """
      # ROLE: Validation, Data Integrity Guarantor, Security.
 
     # RESPONSIBILITIES:
-    1.  Ensure a Formation instance is certified safe, reliable and consistent before use.
+    1.  Ensure a FormationSuperKey instance is certified safe, reliable and consistent before use.
     2.  If verification fails indicate the reason in an exception, returned to the caller.
 
     # PARENT:
@@ -36,7 +34,7 @@ class OrderContextValidator(Validator[OrderContext]):
     # INHERITED ATTRIBUTES:
     None
     """
-    
+
     @classmethod
     @LoggingLevelRouter.monitor
     def validate(
@@ -44,13 +42,13 @@ class OrderContextValidator(Validator[OrderContext]):
             candidate: Any,
             color_validator: GameColorValidator = GameColorValidator(),
             identity_service: IdentityService = IdentityService(),
-    ) -> ValidationResult[OrderContext]:
+    ) -> ValidationResult[FormationSuperKey]:
         """
         # Action:
         1.  Confirm that only one in the (designation, square_designation, color) tuple is not null.
         2.  Certify the not-null attribute is safe using the appropriate service's validator.
         3.  If any check fails return a ValidationResult containing the exception raised by the failure.
-        4.  On success Build an OrderContext are return in a ValidationResult.
+        4.  On success Build an FormationSuperKey are return in a ValidationResult.
 
         # Parameters:
             *   candidate (Any)
@@ -58,47 +56,47 @@ class OrderContextValidator(Validator[OrderContext]):
             *   identity_service (IdentityService)
 
         # Returns:
-        ValidationResult[OrderContext] containing either:
-            - On success: OrderContext in the payload.
+        ValidationResult[FormationSuperKey] containing either:
+            - On success: FormationSuperKey in the payload.
             - On failure: Exception.
 
         # Raises:
             *   TypeError
-            *   NullOrderContextException
-            *   ZeroOrderContextFlagsException
-            *   ExcessiveOrderContextFlagsException
-            *   InvalidOrderContextException
+            *   NullFormationSuperKeyException
+            *   ZeroFormationSuperKeyFlagsException
+            *   ExcessiveFormationSuperKeyFlagsException
+            *   InvalidFormationSuperKeyException
         """
-        method = "OrderContextValidator.validate"
+        method = "FormationSuperKeyValidator.validate"
         try:
             # If the candidate is null no other checks are needed.
             if candidate is None:
                 return ValidationResult.failure(
-                    NullOrderContextException(f"{method}: {NullOrderContextException.DEFAULT_MESSAGE}")
+                    NullFormationSuperKeyException(f"{method}: {NullFormationSuperKeyException.DEFAULT_MESSAGE}")
                 )
-            # If the candidate is not an OrderContext validation has failed.
-            if not isinstance(candidate, OrderContext):
+            # If the candidate is not an FormationSuperKey validation has failed.
+            if not isinstance(candidate, FormationSuperKey):
                 return ValidationResult.failure(
-                    TypeError(f"{method}: Expected OrderContext, got {type(candidate).__designation__} instead.")
+                    TypeError(f"{method}: Expected FormationSuperKey, got {type(candidate).__designation__} instead.")
                 )
-            
-            # Once existence and type checks are passed, cast the candidate to Formation and run structure tests.
-            context = cast(OrderContext, candidate)
-            
+
+            # Once existence and type checks are passed, cast the candidate to FormationSuperKey and run structure tests.
+            context = cast(FormationSuperKey, candidate)
+
             # Handle the case of searching with no attribute-value.
             if len(context.to_dict()) == 0:
                 return ValidationResult.failure(
-                    NoOrderContextFlagException(f"{method}: {NoOrderContextFlagException.DEFAULT_MESSAGE}")
+                    NoFormationSuperKeyFlagException(f"{method}: {NoFormationSuperKeyFlagException.DEFAULT_MESSAGE}")
                 )
             # Handle the case of too many attributes being used in a search.
             if len(context.to_dict()) > 1:
                 return ValidationResult.failure(
-                    ExcessiveOrderContextFlagsException(
-                        f"{method}: {ExcessiveOrderContextFlagsException.DEFAULT_MESSAGE}"
+                    ExcessiveFormationSuperKeyFlagsException(
+                        f"{method}: {ExcessiveFormationSuperKeyFlagsException.DEFAULT_MESSAGE}"
                     )
                 )
             # When structure tests are passed certify whichever search value was provided.
-            
+
             # Certification for the search-by-designation target.
             if context.designation is not None:
                 validation = identity_service.validate_name(candidate=context.designation)
@@ -106,7 +104,7 @@ class OrderContextValidator(Validator[OrderContext]):
                     return ValidationResult.failure(validation.exception)
                 # On certification success return the battle_order.designation map in a ValidationResult.
                 return ValidationResult.success(context)
-            
+
             # Certification for the search-by-square_name target.
             if context.square is not None:
                 validation = identity_service.validate_name(candidate=context.square)
@@ -114,7 +112,7 @@ class OrderContextValidator(Validator[OrderContext]):
                     return ValidationResult.failure(validation.exception)
                 # On certification success return the battle_order.square_name map in a ValidationResult.
                 return ValidationResult.success(context)
-            
+
             # Certification for the search-by-color target.
             if context.color is not None:
                 validation = color_validator.validate(candidate=context.color)
@@ -122,11 +120,11 @@ class OrderContextValidator(Validator[OrderContext]):
                     return ValidationResult.failure(validation.exception)
                 # On certification success return the battle_order.color map in a ValidationResult.
                 return ValidationResult.success(context)
-        
-        # Finally, catch any missed exception, wrap an InvalidOrderContextException. Then send the exception-chain in a ValidationResult.
+
+        # Finally, catch any missed exception, wrap an InvalidFormationSuperKeyException. Then send the exception-chain in a ValidationResult.
         except Exception as ex:
             return ValidationResult.failure(
-                InvalidOrderContextException(
-                    ex=ex, message=f"{method}: {InvalidOrderContextException.DEFAULT_MESSAGE}"
+                InvalidFormationSuperKeyException(
+                    ex=ex, message=f"{method}: {InvalidFormationSuperKeyException.DEFAULT_MESSAGE}"
                 )
             )
