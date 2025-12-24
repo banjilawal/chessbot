d47372831c10df02111bdb9070ce2e2d98cc90bd
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-23 22:18:07 -0600

    Wrote SchemaAgentPairBuildFaieldException.

--- src/chess/dyad/builder/exception.py
+++ src/chess/dyad/builder/exception.py
@@ -10,5 +10,35 @@ version: 1.0.0
 from chess.system import BuildFailedException
 from chess.dyad import SchemaAgentPairException
 
+
+__all__ = [
+    # ======================# SCHEMA_AGENT_PAIR_BUILD_FAILED EXCEPTION #======================#
+    "SchemaAgentPairBuildFailedException",
+]
+
+
+# ======================# SCHEMA_AGENT_PAIR_BUILD_FAILED EXCEPTION #======================#
 class SchemaAgentPairBuildFailedException(SchemaAgentPairException, BuildFailedException):
-    pass
+    """
+    # ROLE: Exception Wrapper
+
+    # RESPONSIBILITIES:
+    1.  Any failed check during the SchemaAgentPair build creates an exception. Failed check exceptions are encapsulated
+        in an SchemaAgentPairBuildFailedException which is sent to the caller in a BuildResult.
+    2.  The SchemaAgentPairBuildFailedException provides a trace for debugging and application recovery.tion recovery.
+
+    # PARENT:
+        *   SchemaAgentPairException
+        *   BuildFailedException
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    # INHERITED ATTRIBUTES:
+    None
+    """
+    ERROR_CODE = "SCHEMA_AGENT_PAIR_BUILD_FAILE_ERROR"
+    DEFAULT_MESSAGE = "SchemaAgentPair build failed."
