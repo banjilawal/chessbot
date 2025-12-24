bc6d2957b4eddb98e9413f80fe1315088b5bddf9
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-24 07:53:32 -0600

    Updated the InvalidFormationException pydoc.

--- src/chess/formation/validator/exception/base.py
+++ src/chess/formation/validator/exception/base.py
@@ -11,31 +11,36 @@ version: 1.0.0
 from chess.formation import FormationException
 from chess.system import ValidationFailedException
 
+_
 __all__ = [
-    # ======================# FORMATION_VALIDATION EXCEPTION #======================#
+    # ======================# FORMATION_VALIDATION_FAILURE EXCEPTION #======================#
     "InvalidFormationException",
 ]
 
 
-# ======================# FORMATION_VALIDATION EXCEPTION #======================#
+# ======================# FORMATION_VALIDATION_FAILURE EXCEPTION #======================#
 class InvalidFormationException(FormationException, ValidationFailedException):
     """
     # ROLE: Exception Wrapper, Catchall Exception
 
     # RESPONSIBILITIES:
-    1.  Parent of exceptions raised during an Formation verification process.
-    2.  Catchall Exception for BattleOrderValidator when a candidate fails a sanity check.
-    3.  Wrap an exception that hits the try-finally block of an BattleOrderValidator method.
-
+    1.  A debug exception is created when a Formation candidate fails a validation test. Validation debug exceptions are
+        encapsulated inside an InvalidFormationException creating an exception chain. which is sent tot he caller in a
+        ValidationResult.
+    2.  The InvalidFormationException chain is useful for tracing a  failure to its source.
+    
     # PARENT:
         *   FormationException
         *   ValidationFailedException
 
     # PROVIDES:
-    InvalidFormationException
+    None
 
-    # ATTRIBUTES:
+    # LOCAL ATTRIBUTES:
+    None
+
+    INHERITED ATTRIBUTES:
     None
     """
-    ERROR_CODE = "FORMATION_VALIDATION"
+    ERROR_CODE = "FORMATION_VALIDATION_FAILURE_ERROR"
     DEFAULT_MESSAGE = "Formation validation failed."
