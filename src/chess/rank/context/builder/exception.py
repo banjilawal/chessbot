4c7aa1aeccca8f608e14564491bbad732fa8353f
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-23 22:39:43 -0600

    Wrote RankContextBuildFailedException.

---
+++ src/chess/rank/context/builder/exception.py
@@ -0,0 +1,31 @@
+__all__ = [
+    # ======================# RANK_CONTEXT_BUILD_FAILED EXCEPTION #======================#
+    "RankContextBuildFailedException",
+]
+
+
+# ======================# RANK_CONTEXT_BUILD_FAILED EXCEPTION #======================#
+class RankContextBuildFailedException(RankContextException, BuildFailedException):
+    """
+    # ROLE: Exception Wrapper
+
+    # RESPONSIBILITIES:
+    1.  Any failed check during the RankContext build creates an exception. Failed check exceptions are encapsulated
+        in an RankContextBuildFailedException which is sent to the caller in a BuildResult.
+    2.  The RankContextBuildFailedException provides a trace for debugging and application recovery.tion recovery.
+
+    # PARENT:
+        *   RankContextException
+        *   BuildFailedException
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    # INHERITED ATTRIBUTES:
+    None
+    """
+    ERROR_CODE = "RANK_CONTEXT_BUILD_FAILED_ERROR"
+    DEFAULT_MESSAGE = "RankContext build failed."
--- src/chess/snapshot/context/builder/exception.py
+++ src/chess/snapshot/context/builder/exception.py
@@ -7,24 +7,24 @@ Created: 2025-10-03
 version: 1.0.0
 """
 
-
 from chess.system import BuildFailedException
-from chess.game import SnapshotContextException
+from chess.snapshot import SnapshotContextException
 
 __all__ = [
-    # ======================# SNAPSHOT_CONTEXT BUILD EXCEPTION #======================#
+    # ======================# SNAPSHOT_CONTEXT_BUILD_FAILED EXCEPTION #======================#
     "SnapshotContextBuildFailedException",
 ]
 
 
-# ======================# SNAPSHOT_CONTEXT BUILD EXCEPTION #======================#
+# ======================# SNAPSHOT_CONTEXT_BUILD_FAILED EXCEPTION #======================#
 class SnapshotContextBuildFailedException(SnapshotContextException, BuildFailedException):
     """
-    # ROLE: Exception Wrapper, Catchall Exception
+    # ROLE: Exception Wrapper
 
     # RESPONSIBILITIES:
-    1.  Parent of exception raised during SnapshotContext build process.
-    2.  Wraps an exception that hits the try-finally block of an SnapshotContextBuilder method.
+    1.  Any failed check during the SnapshotContext build creates an exception. Failed check exceptions are encapsulated
+        in an SnapshotContextBuildFailedException which is sent to the caller in a BuildResult.
+    2.  The SnapshotContextBuildFailedException provides a trace for debugging and application recovery.tion recovery.
 
     # PARENT:
         *   SnapshotContextException
@@ -39,5 +39,5 @@ class SnapshotContextBuildFailedException(SnapshotContextException, BuildFailedE
     # INHERITED ATTRIBUTES:
     None
     """
-    ERROR_CODE = "SNAPSHOT_CONTEXT_BUILD_ERROR"
+    ERROR_CODE = "SNAPSHOT_CONTEXT_BUILD_FAILED_ERROR"
     DEFAULT_MESSAGE = "SnapshotContext build failed."
