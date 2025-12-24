b3ecbcf754ce6ff63a6e88acb62e30c426255b32
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-23 22:22:11 -0600

    Updated ScalarBuildFailedException.

--- src/chess/scalar/builder/exception.py
+++ src/chess/scalar/builder/exception.py
@@ -10,18 +10,34 @@ version: 1.0.0
 from chess.scalar import ScalarException
 from chess.system import BuildFailedException
 
-
 __all__ = [
-    #======================# SCALAR BUILD EXCEPTION #======================#
+    # ======================# SCALAR_BUILD_FAILED EXCEPTION #======================#
     "ScalarBuildFailedException",
 ]
 
 
-#======================# SCALAR BUILD EXCEPTION #======================#
+# ======================# SCALAR_BUILD_FAILED EXCEPTION #======================#
 class ScalarBuildFailedException(ScalarException, BuildFailedException):
     """
-    Catchall/wrapper exception for when a condition not handled directly by ScalarBuilder 
-    prevents successful Scalar creation.
+    # ROLE: Exception Wrapper
+
+    # RESPONSIBILITIES:
+    1.  Any failed check during the Scalar build creates an exception. Failed check exceptions are encapsulated
+        in an ScalarBuildFailedException which is sent to the caller in a BuildResult.
+    2.  The ScalarBuildFailedException provides a trace for debugging and application recovery.tion recovery.
+
+    # PARENT:
+        *   ScalarException
+        *   BuildFailedException
+
+    # PROVIDES:
+    None
+
+    # LOCAL ATTRIBUTES:
+    None
+
+    # INHERITED ATTRIBUTES:
+    None
     """
     ERROR_CODE = "SCALAR_BUILD_FAILED_ERROR"
     DEFAULT_MESSAGE = "Scalar build failed."
