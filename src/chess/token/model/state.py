1bf96aef28bed5446de42c7ea82b11a4470202e0
Banji Lawal <banji.k.lawal@gmail.com>
2025-12-29 12:25:02 -0600

    1) Wrote AgentTeamRelationTester, AgentTeamRelationTestfailedException.2) Added AgentTeamRelationtester to AgentService.3) Wrote AgentService.add_team.3) Updated method names in RelationReport.4) Changed Generic profile for RelationReport.5) Wrote ArenaTeamRelationTester and ArenaTeamRelationTestFailedException inside chess.arena.arena.relation.6) Added AgentServie.add_team.7) Documented AgentTeamRelationTester and ArenaTeamRelationTester.8) Created chess.team.relation.{member, hostage} packagges. Both packages contain an RelationTester and exception.9) Went through chess.token replacing the the piece with token.10) Created ches.token.relation package.11) Added chess.team.service.location.TokenLocation.11) Renamed chess.token.model.state.TokenState to ActivityState.

--- src/chess/token/model/state.py
+++ src/chess/token/model/state.py
@@ -1,11 +1,12 @@
 from enum import Enum, auto
 
 
-class TokenState(Enum):
+class ActivityState(Enum):
     ACTIVE_KING = auto(),
     KING_IN_CHECK = auto(),
     CHECKMATED_KING = auto(),
     ACTIVE_COMBATANT = auto(),
     CAPTURED_COMBATANT = auto(),
+    BLOCKED_COMBATANT = auto(),
     PROMOTED_PAWN = auto(),
     NOT_OPENED = auto(),
