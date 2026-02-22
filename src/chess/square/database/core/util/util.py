e17ff13896bb3f066e55b8cac99d2a2926c00063
Banji Lawal <banji.k.lawal@gmail.com>
2026-02-22 01:40:06 -0600

    Added RosterFormationCoordinator to SquareStackUtil.

--- src/chess/square/database/core/util/util.py
+++ src/chess/square/database/core/util/util.py
@@ -8,17 +8,25 @@ version: 1.0.0
 """
 
 from chess.square import SquareStackAnalyzer
+from chess.square.database.core.util.roster.deployer import RosterFormationCoordinator
 
 
 class SquareStackUtil:
     _stats_analyzer: SquareStackAnalyzer
+    _roster_deployer: RosterFormationCoordinator
     
     def __init__(
             self,
             stats_analyzer: SquareStackAnalyzer = SquareStackAnalyzer(),
+            roster_deployer: RosterFormationCoordinator = RosterFormationCoordinator(),
     ):
         self._stats_analyzer = stats_analyzer
+        self._roster_deployer = roster_deployer
         
     @property
     def stats_analyzer(self) -> SquareStackAnalyzer:
         return self._stats_analyzer
+    
+    @property
+    def roster_deployer(self) -> RosterFormationCoordinator:
+        return self._roster_deployer
