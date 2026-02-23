b0786ce1216d373b4ec9e5a5b8c9ac85bdf835a8
Banji Lawal <banji.k.lawal@gmail.com>
2026-02-22 22:38:11 -0600

    Wrote pydoc for TokenVisitHandlerException.

--- src/chess/square/service/visitation/exception/catchall.py
+++ src/chess/square/service/visitation/exception/catchall.py
@@ -8,22 +8,22 @@ version: 1.0.0
 """
 
 
-from chess.system import CatchallException, ServiceException
-
 __all__ = [
     # ======================# TOKEN_VISIT_HANDLER EXCEPTION #======================#
     "TokenVisitHandlerException",
 ]
 
+from chess.system import CatchallException
+
 
 # ======================# TOKEN_VISIT_HANDLER EXCEPTION #======================#
 class TokenVisitHandlerException(CatchallException):
     """"
-    # ROLE: Catchall, Exception Messaging
-    
+    # ROLE: Class/Module Identifier, Exception Chain Layer 3, Exception Messaging
+
     # RESPONSIBILITIES:
-    1.  Outermost layer of the 3-part exception chain that is created when a TokenVisitHandler
-        operation aborts.
+    1.  Indicate a failure occurred in TokenVisitHandler.
+    2.  The method where the error occurred is identified in the exception nested directly underneath.
 
     # PARENT:
         *   CatchallException
