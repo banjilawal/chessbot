5e64c7cee6baeec8828ce61a154fef6cc1b143e6
Banji Lawal <banji.k.lawal@gmail.com>
2026-02-22 19:36:25 -0600

    1) Simplified CollectionOperationException's inheritcane.2) Rewrote CollectionOperationException, using OperationException as a template.

--- src/chess/system/collection/operation/exception.py
+++ src/chess/system/collection/operation/exception.py
@@ -7,7 +7,7 @@ Created: 2025-11-18
 Version: 1.0.0
 """
 
-from chess.system import CollectionException, OperationException
+from chess.system import OperationException
 
 __all__ = [
     # ======================# COLLECTION_OPERATION_FAILURE #======================#
@@ -16,15 +16,15 @@ __all__ = [
 
 
 # ======================# COLLECTION_OPERATION_FAILURE #======================#
-class CollectionOperationException(CollectionException, OperationException):
+class CollectionOperationException(OperationException):
     """
-    # ROLE: Exception Wrapper
+    # ROLE: Debug Wrapper, Exception Chain Layer 2, Exception Messaging
 
     # RESPONSIBILITIES:
-    1.  Wraps collection operation debug exceptions to create an exception chain.
+    1.  Indicate what Collection Operation failed.
+    2.  Encapsulate the DebugException which describes the failure condition.
 
     # PARENT:
-        *   CollectionOperation
         *   OperationException
 
     # PROVIDES:
@@ -37,4 +37,4 @@ class CollectionOperationException(CollectionException, OperationException):
     None
     """
     ERROR_CODE = "COLLECTION_OPERATION_FAILURE"
-    DEFAULT_MESSAGE = "Calculation failed."
+    DEFAULT_MESSAGE = "Calculation operation failed."
