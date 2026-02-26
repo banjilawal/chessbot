b3dc709128e053f30deee211edc2d00fb740f410
Banji Lawal <banji.k.lawal@gmail.com>
2026-02-25 20:20:53 -0600

    Moved DebugExceptio to system.err.debug.

---
+++ src/chess/system/err/debug/child/__init__.py
@@ -0,0 +1,15 @@
+# src/chess/system/err/debug/child/__init__.py
+
+"""
+Module: chess.system.err.debug.child.__init__
+Author: Banji Lawal
+Created: 2026-02-25
+version: 1.0.0
+"""
+
+# =========== SYSTEM.ERR.DEBUG.CHILD PACKAGE CONTENTS ===========#
+
+# Packages
+None
+
+# Modules
