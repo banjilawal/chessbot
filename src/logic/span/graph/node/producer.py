774d6d3c7fc5a0de9160b885c3b3e38982d2b702
Banji Lawal <banji.k.lawal@gmail.com>
2026-03-12 18:18:37 -0500

    Wrote pydocs for NodeTreeProducer methods: _get_sub_span_roots, _produce_node_pair_list, _produce_node_pair.

--- src/logic/span/graph/node/producer.py
+++ src/logic/span/graph/node/producer.py
@@ -30,10 +30,17 @@ class NodePair:
 
 @dataclass    
 class NodePairList:
+    """
+    NodePairList's physical structure can be either
+    a list or tree.
+    """
     items: List[NodePair]
 
 @dataclass    
 class NodeTree:
+    """
+    The tree might have one root or subtrees.
+    """
     root: Node
     branches: List[NodePairList]
 
@@ -94,34 +101,34 @@ class NodeTreeProducer:
     @LoggingLevelRouter.monitor
     def _production_helper(
             cls,
-            origin_node: Node,
+            tree_origin: Node,
             square_span: SquareSpan,
             node_service: NodeService,
     ) -> BuildResult[NodeTree]:
         """
+        Action:
+            1.  If there are sub-spans create an innermost span of their roots which is appended
         Args:
-            origin_node: Node
+            span_origin_node: Node
             square_span: SquareSpan
             node_service: NodeService
-            
         Returns:
             BuildResult[NodeTree]
-            
         Raises:
             NodeStackServiceProductionException
         """
         method = f"{cls.__class__.__name__}._production_helper"
         
-        node_tree = NodeTree(root=origin_node, branches=[])
-        
-        inner_ray = cls._inner_ring_ray(square_span=square_span)
-        if len(inner_ray.members) > 0:
-            square_span.rays.append(inner_ray)
+        # --- If the span contains any child spans, put them in a ray, then append to the parent, rays. ---#
+        sub_span_roots= cls._get_sub_span_roots(square_span=square_span)
+        if not sub_span_roots.is_empty:
+            square_span.rays.append(sub_span_roots)
             
+        node_tree = NodeTree(root=tree_origin, branches=[])
         for ray in square_span.rays:
             node_pair_list_build_result = cls._produce_node_ray(
                 square_ray=ray,
-                origin_node=origin_node,
+                origin_node=tree_origin,
                 node_service=node_service,
             )
             # Handle the case that the node_link_pair is not built.
@@ -142,58 +149,83 @@ class NodeTreeProducer:
             
     @classmethod
     @LoggingLevelRouter.monitor
-    def _inner_ring_ray(cls, square_span: SquareSpan,) -> SquareRay:
+    def _get_sub_span_roots(cls, square_span: SquareSpan,) -> SquareRay:
         """
-        If any ray's origin differs from the span's origin, put it in an
-        inner_ring_ray.
-        
+        Action:
+            1.  Go through the rays looking for any ray.origin != span.origin
+                which indicates there's a child span.
+            2.  If there is a child span put its root into child_span_roots
+                which, is the span's innermost ray.
+            3.  Return the ray of child_span_roots to the caller.
+            
         Args:
             square_span: SquareSpan
-        
+            
         Returns:
             SquareRay
             
         Raises:
             None
         """
-        method = f"{cls.__class__.__name__}._inner_ray"
+        method = f"{cls.__class__.__name__}._get_sub_span_roots"
         
-        inner_ring_ray = SquareRay(origin=square_span.origin, members=[])
+        # --- Create the return target. ---#
+        sub_span_roots_ray = SquareRay(origin=square_span.origin, members=[])
+        
+        # Iterate through the rays looking for child_span roots.
         for ray in square_span.rays:
             if ray.origin != square_span.origin:
-                inner_ring_ray.members.append(ray.origin)
-        return inner_ring_ray
+                sub_span_roots_ray.members.append(ray.origin)
+        # --- Return the innermost ray containing, the child-span roots. ---#
+        return sub_span_roots_ray
 
     @classmethod
     @LoggingLevelRouter.monitor
-    def _produce_node_ray(
+    def _produce_node_pair_list(
             cls,
             origin_node: Node,
             square_ray: SquareRay,
             node_service: NodeService,
     ) -> BuildResult[NodePairList]:
         """
+        Action:
+            1.  Send an exception chain if, either
+                    *   origin_node.square != square_ray.origin
+                    *   a node_pair build fails.
+            2.  Otherwise, start the cursor at the origin_node then, iterate through
+                the ray's members.
+            3.  On each iteration
+                    *   Build a node pair.
+                    *   Append to the node_pair_list.
+                    *   Advance the cursor.
+            4.  Return the success result.
+            
         Args:
             origin_node: Node
             square_ray: SquareRay
-        
+            node_service: NodeService
+            
         Returns:
-            BuildResult[List[Dict[str, Node]]]
-        
+            BuildResult[NodePairList]
+            
         Raises:
             NodeStackServiceProductionException
         """
-        method = f"{cls.__class__.__name__}._produce_node_ray"
+        method = f"{cls.__class__.__name__}._produce_node_pair_list"
         
+        # --- Create the cursor and return target. ---#
+        cursor = origin_node
         node_pair_list: NodePairList = NodePairList()
+
+        # --- Do the node_pair building work on each ray member. ---#
         for member in square_ray.members:
-            node_link_build_result = cls._build_node_pair(
-                square=member,
-                origin_node=origin_node,
+            production_result = cls._produce_node_pair(
+                head=cursor,
+                tail_square=member,
                 node_service=node_service,
             )
-            # Handle the case that the node_link_pair is not built.
-            if node_link_build_result.is_failure:
+            # Handle the case that, there is no work product.
+            if production_result.is_failure:
                 # Return the exception chain on failure.
                 BuildResult.failure(
                     NodeStackServiceProductionException(
@@ -202,45 +234,50 @@ class NodeTreeProducer:
                         msg=NodeStackServiceProductionException.MSG,
                         err_code=NodeStackServiceProductionException.ERR_CODE,
                         rslt_type=NodeStackServiceProductionException.RSLT_TYPE,
-                        ex=node_link_build_result.exception,
+                        ex=production_result.exception,
                     )
                 )
-                # Add the new node_pairing to the list.
-                node_pair_list.items.append(node_link_build_result.payload)
-                # Update the origin node.
-                origin_node = node_link_build_result.payload.tail
+                # --- Add the pair to the list, then advance the cursor_node. ---#.
+                node_pair_list.items.append(production_result.payload)
+                cursor = production_result.payload.tail
             
-        # --- Send the success result to the caller. ---#
+        # --- Send the completed list of node_pairs ---#
         return BuildResult.success(node_pair_list)
     
     @classmethod
     @LoggingLevelRouter.monitor
-    def _build_node_pair(
+    def _produce_node_pair(
             cls,
             head: Node,
-            square: Square,
+            tail_square: Square,
             node_service: NodeService,
     ) -> BuildResult[NodePair]:
         """
-        Build a node from adjacen
-
+        Action:
+            1.  If building a node from the tail_square fails send an exception chain in
+                the BuildResult.
+            2.  If the build was successful
+                    *   Build a node_pair with the head and its new tail.
+                    *   Return the work product.
         Args:
             head: Node
-            square: Square
+            tail_square: Square
             node_service: NodeService
-
+            
         Returns:
-            BuildResult[Dict[str, Node]]
-
+            BuildResult[NodePair]
+            
         Raises:
             SquareNotFoundException
             NodeStackServiceProductionException
         """
-        method = f"{cls.__class__.__name__}.square_from_coord"
+        method = f"{cls.__class__.__name__}._produce_node_pain"
         
-        node_build_result = node_service.builder.build(square=square,)
-        # Handle the case that the node is not built.
-        if node_build_result.is_failure:
+        # --- Attempt building the tail node. ---#
+        tail_node_build_result = node_service.builder.build(square=tail_square,)
+        
+        # Handle the case that, there is no work product.
+        if tail_node_build_result.is_failure:
             # Return the exception chain on failure.
             BuildResult.failure(
                 NodeStackServiceProductionException(
@@ -249,17 +286,18 @@ class NodeTreeProducer:
                     msg=NodeStackServiceProductionException.MSG,
                     err_code=NodeStackServiceProductionException.ERR_CODE,
                     rslt_type=NodeStackServiceProductionException.RSLT_TYPE,
-                    ex=node_build_result.exception,
+                    ex=tail_node_build_result.exception,
                 )
             )
-        # --- Create the Dictionary of the head and tail node. ---#
-        node_pair = NodePair(head=head, tail=node_build_result.payload)
+        
+        # --- Send the work product. ---#
+        BuildResult.success(NodePair(head=head, tail=tail_node_build_result.payload))
         # node_link_pair: Dict[str, Node] = {
         #     "head": head,
         #     "tail": node_build_result.payload,
         # }
         # --- Send the success result to the caller. ---#
-        return BuildResult.success(node_pair)
+        # return BuildResult.success(node_pair)
         
         # Handle the case that, a square is not found.
         if search_result.is_empty:
