319eaf02f70caefa7de7bf6fb719741d77e70c7c
Banji Lawal <banji.k.lawal@gmail.com>
2026-03-14 17:52:30 -0500

    Updated TokenContextBuildException.

--- src/logic/square/context/builder/exception/wrapper.py
+++ src/logic/square/context/builder/exception/wrapper.py
@@ -1,7 +1,7 @@
-# src/logic/squareContext/context/builder/exception/wrapper.py
+# src/logic/square/builder/exception/wrapper.py
 
 """
-Module: logic.squareContext.context.builder.exception.wrapper
+Module: logic.square.builder.exception.wrapper
 Author: Banji Lawal
 Created: 2025-09-16
 version: 1.0.0
@@ -11,19 +11,20 @@ from __future__ import annotations
 from typing import Optional
 
 __all__ = [
-    # ======================# SQUARE_CONTEXT_BUILD_FAILURE #======================#
-    "SquareContextBuildException",
+    # ======================# SQUARE_BUILD_FAILURE #======================#
+    "SquareBuildException",
 ]
 
 from logic.system import BuildException
 
-# ======================# SQUARE_CONTEXT_BUILD_FAILURE #======================#
-class SquareContextBuildException(BuildException):
+# ======================# SQUARE_BUILD_FAILURE #======================#
+class SquareBuildException(BuildException):
     """
-    # ROLE: Worker Method Identifier, Exception Chain Layer 1, Exception Messaging
+    # ROLE: Worker Method Identification, Exception Chain Layer 1, Exception Messaging
 
     # RESPONSIBILITIES:
-    1.  Identify the SquareContextBuilder method where the process failed.
+    1.  Indicate the SquareBuilder did not produce a valid work product.
+    2.  Identify the SquareBuilder method where the failure occurred.
 
     # PARENT:
         *   BuildException
@@ -32,44 +33,48 @@ class SquareContextBuildException(BuildException):
     None
 
     # LOCAL ATTRIBUTES:
-        *   op (Optional[str])
-        *   rslt_type (Optional[str])
+    None
 
     # INHERITED ATTRIBUTES:
         *   See BuildException class for inherited attributes.
 
     # CONSTRUCTOR PARAMETERS:)
-        *   err_code (str)
-        *   msg (str)
-        *   ex (Optional[Exception])
-        *   mthd (Optional[str])
-        *   op (Optional[str])
-        *   rslt_type (Optional[str])
+        op: Optional[str]
+        ex: Optional[str]
+        msg: Optional[str]
+        mthd: Optional[str]
+        err_code: Optional[str]
+        rslt_type: Optional[str]
 
     # LOCAL METHODS:
-   None
+    None
 
     # INHERITED METHODS:
         *   See BuildException class for inherited methods.
     """
-    ERR_CODE = "SQUARE_CONTEXT_BUILD_FAILURE"
-    MSG = "Failure in SquareContextBuilder method."
-    MTHD = Optional[str]
     OP = "Build"
     RSLT_TYPE = "BuildResult"
-    
-    _op = Optional[str]
-    _rslt_type = Optional[str]
-    
+    ERR_CODE = "SQUARE_BUILD_FAILURE"
+    MSG = "Failure in SquareBuilder method."
+
     def __init__(
             self,
-            err_code: Optional[str] = None,
-            msg: Optional[str] = None,
-            ex: Optional[Exception] = None,
-            mthd: Optional[str] = None,
             op: Optional[str] = None,
+            msg: Optional[str] = None,
+            mthd: Optional[str] = None,
+            ex: Optional[Exception] = None,
+            err_code: Optional[str] = None,
             rslt_type: Optional[str] = None,
     ):
+        """
+        Args:
+            op: Optional[str]
+            ex: Optional[str]
+            msg: Optional[str]
+            mthd: Optional[str]
+            err_code: Optional[str]
+            rslt_type: Optional[str]
+        """
         op = op or self.OP
         msg = msg or self.MSG
         mthd = mthd or self.MTHD
--- src/logic/team/context/builder/exception/wrapper.py
+++ src/logic/team/context/builder/exception/wrapper.py
@@ -1,7 +1,7 @@
-# src/logic/teamContext/context/builder/exception/wrapper.py
+# src/logic/team/factory/exception/wrapper.py
 
 """
-Module: logic.teamContext.context.builder.exception.wrapper
+Module: logic.team.factory.exception.wrapper
 Author: Banji Lawal
 Created: 2025-09-16
 version: 1.0.0
@@ -11,20 +11,20 @@ from __future__ import annotations
 from typing import Optional
 
 __all__ = [
-    # ======================# TEAM_CONTEXT_BUILD_FAILURE #======================#
-    "TeamContextBuildException",
+    # ======================# TEAM_BUILD_FAILURE #======================#
+    "TeamBuildException",
 ]
 
 from logic.system import BuildException
 
-# ======================# TEAM_CONTEXT_BUILD_FAILURE #======================#
-class TeamContextBuildException(BuildException):
+# ======================# TEAM_BUILD_FAILURE #======================#
+class TeamBuildException(BuildException):
     """
-    # ROLE: Exception Chain Layer 1, Exception Messaging
-    # TASK: Worker Method Identifier
+    # ROLE: Worker Method Identification, Exception Chain Layer 1, Exception Messaging
 
     # RESPONSIBILITIES:
-    1.  Identify the TeamContextBuilder method where the process failed.
+    1.  Indicate the TeamBuilder did not produce a valid work product.
+    2.  Identify the TeamBuilder method where the failure occurred.
 
     # PARENT:
         *   BuildException
@@ -33,44 +33,48 @@ class TeamContextBuildException(BuildException):
     None
 
     # LOCAL ATTRIBUTES:
-        *   op (Optional[str])
-        *   rslt_type (Optional[str])
+    None
 
     # INHERITED ATTRIBUTES:
         *   See BuildException class for inherited attributes.
 
     # CONSTRUCTOR PARAMETERS:)
-        *   err_code (str)
-        *   msg (str)
-        *   ex (Optional[Exception])
-        *   mthd (Optional[str])
-        *   op (Optional[str])
-        *   rslt_type (Optional[str])
+        op: Optional[str]
+        ex: Optional[str]
+        msg: Optional[str]
+        mthd: Optional[str]
+        err_code: Optional[str]
+        rslt_type: Optional[str]
 
     # LOCAL METHODS:
-   None
+    None
 
     # INHERITED METHODS:
         *   See BuildException class for inherited methods.
     """
-    ERR_CODE = "TEAM_CONTEXT_BUILD_FAILURE"
-    MSG = "Failure in TeamContextBuilder method."
-    MTHD = Optional[str]
     OP = "Build"
     RSLT_TYPE = "BuildResult"
-    
-    _op = Optional[str]
-    _rslt_type = Optional[str]
-    
+    ERR_CODE = "TEAM_BUILD_FAILURE"
+    MSG = "Failure in TeamBuilder method."
+
     def __init__(
             self,
-            err_code: Optional[str] = None,
-            msg: Optional[str] = None,
-            ex: Optional[Exception] = None,
-            mthd: Optional[str] = None,
             op: Optional[str] = None,
+            msg: Optional[str] = None,
+            mthd: Optional[str] = None,
+            ex: Optional[Exception] = None,
+            err_code: Optional[str] = None,
             rslt_type: Optional[str] = None,
     ):
+        """
+        Args:
+            op: Optional[str]
+            ex: Optional[str]
+            msg: Optional[str]
+            mthd: Optional[str]
+            err_code: Optional[str]
+            rslt_type: Optional[str]
+        """
         op = op or self.OP
         msg = msg or self.MSG
         mthd = mthd or self.MTHD
