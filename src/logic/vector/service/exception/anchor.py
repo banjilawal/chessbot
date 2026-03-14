3240075633f82c4474123a6f360b6c3a04c7deeb
Banji Lawal <banji.k.lawal@gmail.com>
2026-03-14 12:57:44 -0500

    USed EdgeSException to update VectorServiceException.

--- src/logic/vector/service/exception/anchor.py
+++ src/logic/vector/service/exception/anchor.py
@@ -1,4 +1,4 @@
-# src/logic/vector/service/exception.anchor.py
+# src/logic/vector/service/exception/anchor.py
 
 """
 Module: logic.vector.service.exception.anchor
@@ -11,23 +11,23 @@ from __future__ import annotations
 from typing import Optional
 
 __all__ = [
-    # ======================# VECTOR_EXCEPTION #======================#
+    # ======================# VECTOR_SERVICE_EXCEPTION #======================#
     "VectorServiceException",
 ]
 
 from logic.system import AnchorException
 
 
-# ======================# VECTOR_EXCEPTION #======================#
+# ======================# VECTOR_SERVICE_EXCEPTION #======================#
 class VectorServiceException(AnchorException):
     """
     # ROLE: Coverage Target, Exception Chain Layer 0
 
     # RESPONSIBILITIES:
-    1.  Provide VectorService as:
-            *   Reporting
-            *   Coverage
-        target for layer-2 debugging exceptions.
+    1.  Anchoring target for VectorService debug (layer-2) error state firing incident
+        reports on
+            *   the triggering variable
+            *   The trigger's value.
     2.  Indicate which VectorService method received a worker's (layer-1) failure result.
 
     # PARENT:
@@ -43,11 +43,11 @@ class VectorServiceException(AnchorException):
         *   See AnchorException class for inherited attributes.
 
     # CONSTRUCTOR PARAMETERS:
-        *   msg (str)
-        *   err_code (str)
-        *   ex (Optional[Exception])
-        *   cls_name (Optional[str])
-        *   cls_mthd (Optional[str])
+        msg: Optional[str]
+        ex: Optional[Exception]
+        cls_name: Optional[str]
+        cls_mthd: Optional[str]
+        err_code: Optional[str]
 
     # LOCAL METHODS:
     None
@@ -55,35 +55,27 @@ class VectorServiceException(AnchorException):
     # INHERITED METHODS:
         *   See AnchorException class for inherited methods.
     """
-    CLS_MTHD = Optional[str]
-    CLS_NAME = "VectorService"
-    ERR_CODE = "VECTOR_EXCEPTION"
-    MSG = "Exception raised in VectorService"
+    CLS_NAME = " VectorService"
+    ERR_CODE = " VECTOR_SERVICE_EXCEPTION"
+    MSG = " VectorService raised an exception."
     
     def __init__(
             self,
             msg: Optional[str] = None,
-            err_code: Optional[str] = None,
             ex: Optional[Exception] = None,
             cls_name: Optional[str] = None,
             cls_mthd: Optional[str] = None,
+            err_code: Optional[str] = None,
     ):
         """
         Args:
             msg: Optional[str]
-            cls_mthd: Optional[str]
-            cls_name: Optional[str
-            err_code: Optional[str]
             ex: Optional[Exception]
+            cls_name: Optional[str]
+            cls_mthd: Optional[str]
+            err_code: Optional[str]
         """
         msg = msg or self.MSG
-        err_code = err_code or self.ERR_CODE
         cls_name = cls_name or self.CLS_NAME
-        cls_mthd = cls_mthd or self.CLS_MTHD
-        
-        super().__init__(
-            ex=ex, msg=msg,
-            err_code=err_code,
-            cls_name=cls_name,
-            cls_mthd=cls_mthd
-        )
+        err_code = err_code or self.ERR_CODE
+        super().__init__(msg=msg, err_code=err_code, ex=ex, cls_name=cls_name, cls_mthd=cls_mthd)
