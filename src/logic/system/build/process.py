c0a724e081c455b8e9fa6d11809dda854c29810e
Banji Lawal <banji.k.lawal@gmail.com>
2026-03-29 12:44:26 -0500

    wrote notes about workers and their inside Builder.

--- src/logic/system/build/process.py
+++ src/logic/system/build/process.py
@@ -22,7 +22,33 @@ class Builder(ABC, Generic[T]):
         -   Integrity Maintenance
         -   Consistency Assurance
         -   Process Runner
+       
+    Workers:
+        -   Workers are the logic that implements a process.
+        -   Processes produce a result.
+        
+        Naming Problems:
+            -   Don't have a good naming convention for workers yet. Maybe <ProcessName>Worker? as default,
+                if it's not clunky.
+            -   Worker entry points are named execute. Might change them to work() and the work executes the process.
+            
+            Exceptions:
+                -   Exceptions are only about the process.
+                -   Exception naming convention is <ProcessName>Exception.
 
+        Transaction Worker:
+            -   Work on stateful data objects.
+            -   Include a data object's previous state in their work product.
+            -   For simplicity, speed, and side-effect avoidance use deep copy before state changing logic.
+            -   Client uses original data for rollbacks if transaction fails.
+   
+        Build Worker Naming:
+            -   Builders are a core workers in the system.
+            -   The least clunky and intuitive name is <EntityName>Builder.
+            -   Developers might assume other process exceptions are about the worker and use confusing naming
+                conventions for other processes and their exceptions.
+            -   Despite the possible confusion <EntityName>Builder naming is worth the trade off.
+        
     Responsibilities:
         1.  Creation process owners.
         2.  Execute binding logic for related entities.
