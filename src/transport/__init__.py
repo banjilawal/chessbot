6abaebfe01f56ae4f0477d71e55c19541c402d71
Banji Lawal <banji.k.lawal@gmail.com>
2026-03-30 17:41:02 -0500

    Created .transport package.

---
+++ src/transport/__init__.py
@@ -0,0 +1,22 @@
+# src/transport/__init__.py
+
+"""
+Module: transport.__init__
+Author: Banji Lawal
+Created: 2026-03-01
+version: 1.0.0
+"""
+
+import logging
+
+log = logging.getLogger("chessbot")
+
+# =========== PACKAGE CONTENTS ===========#
+
+# Packages
+from .request import *
+from .response import *
+from .route import *
+
+# Modules
+None
