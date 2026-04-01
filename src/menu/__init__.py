bd3c29b0c02feb5fc84067f7754652847010078b
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-01 00:10:17 -0500

    Created src/menu package.

---
+++ src/menu/__init__.py
@@ -0,0 +1,20 @@
+# src/menu/__init__.py
+
+"""
+Module: menu.__init__
+Author: Banji Lawal
+Created: 2026-03-30
+version: 1.0.0
+"""
+
+import logging
+
+log = logging.getLogger("chessbot")
+
+# =========== PACKAGE CONTENTS ===========#
+
+# Packages
+None
+
+# Modules
+None
