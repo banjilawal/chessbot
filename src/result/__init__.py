d44537c0698dc3d9e21021a4987271956ca840af
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-03 15:13:21 -0500

    Made .result.adt package.

---
+++ src/result/__init__.py
@@ -0,0 +1,20 @@
+# src/result/__init__.py
+
+"""
+Module: menu.__init__
+Author: Banji Lawal
+Created: 2026-03-30
+version: 1.0.1
+"""
+
+import logging
+
+log = logging.getLogger("chessbot")
+
+# =========== PACKAGE CONTENTS ===========#
+
+# Packages
+from .system import *
+
+# Modules
+None
