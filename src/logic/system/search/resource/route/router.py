d9e4583f2a20e132adde433d30cd35327c742d24
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-02 21:18:05 -0500

    Wrote SearchRoute pydoc.

--- src/logic/system/search/resource/route/router.py
+++ src/logic/system/search/resource/route/router.py
@@ -18,6 +18,28 @@ T = TypeVar("T")
 
 
 class SearchRouter(Router, Generic[T]):
+    """
+    Role
+        -   Routing
+        -   Stateless Search Engine
+
+   Responsibilities:
+        1.  Process a search query which contains:
+                -   The dataset
+                -   Search criteria
+        2.  Authoritative single-source-of-truth for search results.
+
+    Attributes:
+
+    Provides:
+        -   def route(
+                    query: Query[T],
+                    query_validator: Validator[Query[T]]
+            ) -> SearchResult[List[T]]
+
+     Super Class:
+         Builder
+     """
     
     @classmethod
     @abstractmethod
