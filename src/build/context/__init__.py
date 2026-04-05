935ec2afd1f3eb552d8d176ff48214cbae7efd32
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-05 10:02:24 -0500

    Moved context builders to context package.

---
+++ src/build/context/__init__.py
@@ -0,0 +1,16 @@
+# src/build/context/__init__.py
+
+"""
+Module: build.context.__init__
+Author: Banji Lawal
+Created: 2026-04-03
+version: 1.0.1
+"""
+
+# =========== CONTEXT PACKAGE CONTENTS ===========#
+
+# Packages
+None
+
+# Modules
+None
---
+++ src/build/context/coord/__init__.py
@@ -0,0 +1,15 @@
+# src/logic/coord/context/context/service/operation/build/__init__.py
+
+"""
+Module: logic.coord.context.context.service.operation.build.__init__
+Author: Banji Lawal
+Created: 2025-10-03
+version: 1.0.0
+"""
+
+# =========== COORD.QUERY.CONTEXT.SERVICE.OPERATION.BUILD PACKAGE CONTENTS ===========#
+
+# Packages
+
+# Modules
+from .builder import CoordContextBuilder
