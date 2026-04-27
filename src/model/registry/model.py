bce5720a787acbc37d8a8c367d2bcd43bd6dcfcf
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-27 00:09:36 -0500

    1) Moved WorkerRegistry to model.registry and renmaed its module as model.

--- src/model/registry/model.py
+++ src/model/registry/model.py
@@ -15,11 +15,23 @@ from operation import Operation
 
 
 class WorkerRegistry:
+    _invocation_counters: dict[str, int]
+    _registration_counters: dict[str, int]
     _workers: Dict[str, Dict[str, Operation]]
     
     def __init__(self):
         self._workers = {str: {}}
+        self._invocation_counters = {}
+        self._registration_counters = {}
         
     @property
     def workers(self) -> Dict[str, Dict[str, Operation]]:
         return self._workers
+    
+    @property
+    def invocation_counters(self) -> dict[str, int]:
+        return self._invocation_counters
+    
+    @property
+    def registration_counters(self) -> dict[str, int]:
+        return self._registration_counters
