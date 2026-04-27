851754c46d637a39482673a2a509cdbd75bed7a7
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-27 03:01:30 -0500

    Wrote SearchDomainBootstrapper, SearWorkerBootstrapper and AddWorkerBootstrapper. These all run the santiy checks.

---
+++ src/operation/registry/validation/search/domain/operation.py
@@ -0,0 +1,38 @@
+# src/operation/registry/validation/registration/__ini__.py
+
+"""
+Module: operation.registry.validation.registration.__init__
+Author: Banji Lawal
+Created: 2026-04-03
+version: 1.0.1
+"""
+
+from __future__ import annotations
+
+from typing import List
+
+from err import NullException
+from operation import NameValidator, Operation, ValidationBootstrapper, WorkerRegistryOperation
+from result import ValidationResult
+from system import LoggingLevelRouter
+
+
+class SearchWorkerBootstrapper(WorkerRegistryOperation):
+    
+    @classmethod
+    @LoggingLevelRouter.monitor
+    def execute(
+            cls,
+            names: List[str],
+            name_validator: NameValidator |  None = None,
+    ) -> ValidationResult[int]:
+        method = f"{cls.__name__}.execute"
+        if name_validator is None:
+            name_validator = NameValidator()
+        
+        for name in names:
+            validation_result = name_validator.validate(name)
+            if validation_result.is_failure:
+                return ValidationResult.failure(validation_result.error)
+            
+        return ValidationResult.success(0)
