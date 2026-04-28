98cc7fdeb2037ce88ed49d6472d6232f88eee8ec
Banji Lawal <banji.k.lawal@gmail.com>
2026-04-28 15:05:26 -0500

    Cleaned up util.identifier.auto package.

--- src/util/identifier/auto/__init__.py
+++ src/util/identifier/auto/__init__.py
@@ -12,6 +12,7 @@ version: 1.0.1
 # Packages
 
 # Modules
+from .util import AutoId
 
 
 
--- src/util/identifier/factory/util.py
+++ src/util/identifier/factory/util.py
@@ -1,14 +1,16 @@
-# src/logic/system/identity/id/exception.py
+# src/util/identity/factory/util.py
 
 """
-Module: logic.system.identity.id.build
+Module: util.identity.factory.util
 Author: Banji Lawal
-Created: 2025-09-17
-version: 1.0.0
+Created: 2026-04-03
+version: 1.0.1
 """
 
+from __future__ import annotations
 from typing import Dict
 from threading import Lock
+
 
 class IdFactory:
     _locks: Dict[str, Lock] = {}
@@ -17,13 +19,13 @@ class IdFactory:
     
     @classmethod
     def next_id(cls, class_name: str) -> int:
-        method = "IDFactory.next_id"
+        method = f"{cls.__name__}.next_id"
         
         with cls._global_lock:
             if class_name not in cls._counters:
                 cls._counters[class_name] = 0
                 cls._locks[class_name] = Lock()
-                
+        
         with cls._locks[class_name]:
             cls._counters[class_name] += 1
             return cls._counters[class_name]
