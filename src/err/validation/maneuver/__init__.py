9f161f149ad058433bba7d739c7957545e8a583f
Banji Lawal <banji.k.lawal@gmail.com>
2026-07-03 16:31:37 -0500

    1) Wrote err.null.relation.RelationNullException.2) Wrote ToeknOriginRelationNullException.3) Extracted ManueverValidator from TokenPathValidator's logic.4) Refactored and simplified PathValidator.5) Updated Path and Maneuver toolkits.

---
+++ src/err/validation/maneuver/__init__.py
@@ -0,0 +1,15 @@
+# src/err/validation/coord/__init__.py
+
+"""
+Module: err.validation.coord.__init__
+Author: Banji Lawal
+Created: 2026-04-04
+version: 1.0.1
+"""
+
+# ============ ERR.VALIDATION.COORD PACKAGE ===========#
+
+# Packages
+
+# Modules
+from .exception import CoordValidatorException
