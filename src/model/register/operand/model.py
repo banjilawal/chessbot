97b52790950b48cc3b661064944047060bd9f3b5
Banji Lawal <banji.k.lawal@gmail.com>
2026-07-05 09:39:21 -0500

    Added methods; is_vector_register, is_coord_register and is_inconsisten_register to VectorOperandRegister.

--- src/model/register/operand/model.py
+++ src/model/register/operand/model.py
@@ -8,7 +8,7 @@ version: 1.0.1
 """
 
 from __future__ import annotations
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from model import RegisterContentType, VectorOperand
 
@@ -30,36 +30,36 @@ class VectorOperandRegister:
     Super Class:
         Enum
     """
-    _u: VectorOperand
-    _v: VectorOperand
+    _a: VectorOperand
+    _b: VectorOperand
     _content_type: RegisterContentType
     
     def __init__(
             self,
-            u: VectorOperand,
-            v: VectorOperand,
-            content_type: RegisterContentType
+            a: VectorOperand,
+            b: VectorOperand,
+            content_type: Optional[RegisterContentType] | None = None,
     ):
         """
         Args:
-            u: VectorOperand
-            v: VectorOperand
+            a: VectorOperand
+            b: VectorOperand
             content_type: OperandRegisterContentType
         """
-        self._u = u
-        self._v = v
-        self._content_type = content
+        self._a = a
+        self._b = b
+        self._content_type = content_type
         
     @property
     def a(self) -> VectorOperand:
-        return self._u
+        return self._a
     
     @property
     def b(self) -> VectorOperand:
-        return self._v
+        return self._b
     
     @property
-    def category(self) -> RegisterContentType:
+    def category(self) -> Optional[RegisterContentType]:
         return self._content_type
     
     @property
@@ -69,3 +69,16 @@ class VectorOperandRegister:
     @property
     def to_dict(self) -> Dict[str, Any]:
         return {"u": VectorOperand, "v": VectorOperand}
+    
+    @property
+    def is_vector_register(self) -> bool:
+        return self._a.is_vector and self._b.is_vector
+    
+    @property
+    def is_coord_register(self) -> bool:
+        return self._a.is_coord and self._b.is_coord
+    
+    @property
+    def is_inconsistent(self) -> bool:
+        return not self._a.is_coord and not self._b.is_coord
+    
