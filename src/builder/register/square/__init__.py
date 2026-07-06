3a870f78bedd43d71d1dfaaefe28af78e0b1edcf
Banji Lawal <banji.k.lawal@gmail.com>
2026-07-05 20:41:53 -0500

    1) Created SqureRegister and VectorOperandRegister Builder skeletons.

---
+++ src/builder/register/square/__init__.py
@@ -0,0 +1,15 @@
+# src/builder/register/square/__init__.py
+
+"""
+Module: builder.register.square.__init__
+Author: Banji Lawal
+Created: 2026-04-03
+version: 1.0.1
+"""
+
+# =========== BUILDER.REGISTER.SQUARE PACKAGE ===========#
+
+# Packages
+
+# Modules
+from .builder import SquareRegisterBuilder
