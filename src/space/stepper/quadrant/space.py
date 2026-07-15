d1d7186b5ad2f28a781f7d66f2384981c8343ac3
Banji Lawal <banji.k.lawal@gmail.com>
2026-07-14 21:18:18 -0500

    1) Wrote QuadrantStepper factory method pydocs.

--- src/space/stepper/quadrant/space.py
+++ src/space/stepper/quadrant/space.py
@@ -23,12 +23,43 @@ class QuadrantStepper(Stepper[Quadrant]):
     """
     Role:
         -   Lookup Table
+        
+    Background:
+    1.  Consider the diagonal series: p_1(0,0), p_2(1,1), p_3(2,2), ., p_n(n,n). For any
+            p_i, y_i = x_i.
+        We want to find some invariant only in terms of x that will gives us all the ys.
+    2.  Let us consider x in non-negative integers, {0, 1, 2,3,.,n}
+        we now that
+            x_i <= x_j < x_n.
+    3.  The start of the X sequence is
+            X_0 = 0,
+            X_1 = 1,
+            x_2 = 2
+            x_3 = 3
+       Which reveals: x_j = x_i + (j-i) But we want to express only in terms of x and restrict that i < j
+            when i = 0, xo = x
+            x_i = x_(i-1) + 1
+    5. Since x_i = y_i, we can express the series in terms of x_(i-i)
+            y_i = (x_(i-1) + delta_x) + x_(i-1)
+            y_i = 2x_(i-1)
+    6. In the quadrant(x<0, y>0) delta_x = -1.
+    7. For thr quadrant (x<0, y>0) delta_x = 1.
+    8. So for each quadrant we change the slope.
+    
+    Gives us
+        abs(x_step) = 1,
+        f = x + c where c is the step.
+        g = 2bx + b where b is the slope.
+        so
+        v = ((u.x + c), ((2b)u.x + b)
 
     Responsibilities:
         1.  Produce y from the  range of x in a seres projected from the quadrant's origin.
 
     Attributes:
         ENTRY: Dict[str, NumberRegister]
+        
+        x_
         
         x_step: int
         slope: int
@@ -80,7 +111,7 @@ class QuadrantStepper(Stepper[Quadrant]):
 
         Action:
             1.  Set
-                    x_next = x_current
+                    x_next = x_current + x_step
                     y_next = (2 * slope * y_current) + slope
             2.  If VectorBuilder cannot create a safe Vector from x_next, y_next, send
                 an exception chain in the ComputationResult.
@@ -96,7 +127,7 @@ class QuadrantStepper(Stepper[Quadrant]):
         
         # --- Build a new vector from the old. ---#
         build = self.math.vector.builder.execute(
-            x=current.x,
+            x=current.x + self.x_step,
             y=(2 * current.y * self.slope) + self.slope
         )
         # Handle the case that, the build is not successful.
@@ -119,15 +150,9 @@ class QuadrantStepper(Stepper[Quadrant]):
     @classmethod
     def northeast(cls) -> QuadrantStepper:
         """
-        Action:
-            1.  Produce the points northeast from the origin to the top right corner.
-                from:
-                    *   origin.column, origin.row
-                to:
-                    *   NUMBER_OF_COLUMNS-1, 0
-            2.  Increments
-                    *   x-coordinate by 1 to get to the end of the columns.
-                    *   y-coordinate by -1 to get to the first row.
+        QuadrantStepper for going northeast toward top left corner (0, 0)
+            -   x_step = -1,
+            -   slope = -1
         """
         return cls(
             x_step=cls.ENTRY["northeast"].a,
@@ -137,15 +162,9 @@ class QuadrantStepper(Stepper[Quadrant]):
     @classmethod
     def northwest(cls) -> QuadrantStepper:
         """
-        Action:
-            1.  Produce the points northwest from the origin to the top left corner.
-                from:
-                    *   origin.column, origin.row
-                to:
-                    *    first column, first row
-            2.  Decrements
-                    *   x-coordinate by 1 to get to the first column.
-                    *   y-coordinate by 1 to get to the first row.
+        QuadrantStepper for going northeast toward top right corner (num_rows - 1, 0)
+            -   x_step = 1,
+            -   slope = 1
         """
         return cls(
             x_step=cls.ENTRY["northwest"].a,
@@ -155,15 +174,9 @@ class QuadrantStepper(Stepper[Quadrant]):
     @classmethod
     def southwest(cls) -> QuadrantStepper:
         """
-        Action:
-            1.  Produce the points southwest from the origin to the bottom left corner.
-                from:
-                    *   origin.column, origin.row
-                to:
-                    *   first column, last row
-            2.  Increments
-                    *   x-coordinate by -1 to get to the first column.
-                    *   y-coordinate by 1 to get to the first row.
+        QuadrantStepper for going northeast toward bottom left corner (0, num_columns - 1)
+            -   x_step = -1,
+            -   slope = 1
         """
         return cls(
             x_step=cls.ENTRY["southwest"].a,
@@ -173,15 +186,9 @@ class QuadrantStepper(Stepper[Quadrant]):
     @classmethod
     def southeast(cls) -> QuadrantStepper:
         """
-        Action:
-            1.  Produce the points southeast from the origin to the bottom right corner.
-                from:
-                    *   origin.column, origin.row
-                to:
-                    *   last column, last row
-            2.  Decrements
-                    *   x-coordinate by 1 to get to the end of the columns.
-                    *   y-coordinate by 1 to get to the first row.
+        QuadrantStepper for going northeast toward bottom right corner (num_rows - 1, num_columns - 1)
+            -   x_step = 1,
+            -   slope = 1
         """
         return cls(
             x_step=cls.ENTRY["southeast"].a,
