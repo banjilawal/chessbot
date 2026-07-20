be80094ea3967ed47d6aee6dc8f8e23688f5d292
Banji Lawal <banji.k.lawal@gmail.com>
2026-07-20 11:59:48 -0500

    Wrote a PathRootCertifier for PathValidator.

---
+++ src/primary/model/path/__init__.py
@@ -0,0 +1,15 @@
+# src/certifier/model/path/__init__.py
+
+"""
+Module: certifier.model.path.__init__
+Author: Banji Lawal
+Created: 2026-04-03
+version: 1.0.1
+"""
+
+# =========== CERTIFIER.MODEL.PATH PACKAGE ===========#
+
+# Packages
+
+# Modules
+from .certifier import PathRootCertifier
--- src/validator/model/path/validator.py
+++ src/validator/model/path/validator.py
@@ -1,23 +1,25 @@
 # src/validator/model/path/validator.py
 
 """
-Module: validator.model.path.validator
+Module: validator.model.path.operation
 Author: Banji Lawal
 Created: 2026-04-03
 version: 1.0.1
 """
 
 from __future__ import annotations
-from typing import Any, Optional, cast
 
-from err import CircularPathException, PathValidatorException
+from typing import Any, cast
+
+from err import PathValidatorException
 from model import Path
+from primary import PathRootCertifier
 from result import ValidationResult
-from toolkit import PathToolkit
 from util import LoggingLevelRouter
+from validator import ModelValidator
 
 
-class PathValidator:
+class PathValidator(ModelValidator[Path]):
     """
     Role
         -   Transaction Worker
@@ -29,43 +31,38 @@ class PathValidator:
         1.  Ensure a Path instance is certified safe, reliable and consistent before use.
 
     Attributes:
-        toolkit: Optional[PathToolkit]
-        
+        root_certifier: PathRootCertifier
+
     Provides:
-        -   def execute(candidate: Any,) -> ValidationResult[Path]
+        -   execute(candidate: Any) -> ValidationResult
 
     Super Class:
         ModelValidator
     """
-    _toolkit: PathToolkit
     
-    def __init__(self, toolkit: Optional[PathToolkit] | None = PathToolkit()):
-        """
-        Args:
-            toolkit: Optional[PathToolkit]
-        """
-        self._toolkit = toolkit
+    def __init__(
+            self,
+            root_certifier: PathRootCertifier | None = PathRootCertifier(),
+    ):
+        super().__init__(root_certifier=root_certifier)
         
     @property
-    def toolkit(self) -> PathToolkit:
-        return self._toolkit
+    def root_certifier(self) -> PathRootCertifier:
+        return cast(PathRootCertifier, self.root_certifier)
     
+
     @LoggingLevelRouter.monitor
-    def execute(self, candidate: Any,) -> ValidationResult[Path]:
+    def execute(self, candidate: Any) -> ValidationResult:
         """
         Verify the object is a Path that is safe to use.
 
         Action:
-            1.  Send an exception chain in the ValidationResult any of the cases occur:
-                    -   Candidate is null
-                    -   It's not a Path.
-                    _   An id check fails.
-                    -   Either the origin or destination are not safe square.
-                    -   The origin and destination are the same.
-            2.  Otherwise, send the success result.
+            1.  Send an exception chain in the ValidationResult if the candidate fails a
+                root_certifier test..
+            2.  Otherwise, cast the payload into a Path and send in the success result.
+                success result.
         Args:
             candidate: Any
-            toolkit: PathToolkit
         Returns:
             ValidationResult[Path]
         Raises:
@@ -73,15 +70,9 @@ class PathValidator:
         """
         method = f"{self.__class__.__name__}.execute"
         
-
-        
-        # Handle the case that, the validator is not primed.
-        validator_priming_result = self._toolkit.priming_validator.execute(
-            candidate=candidate,
-            target_model=self._toolkit.model,
-            null_exception=self._toolkit.null_exception,
-        )
-        if validator_priming_result.is_failure:
+        # Handle the case that, the candidate is not safe.
+        certification = self.root_certifier.execute(candidate)
+        if certification.is_failure:
             # Send the exception chain on failure.
             return ValidationResult.failure(
                 PathValidatorException(
@@ -89,57 +80,13 @@ class PathValidator:
                     cls_name=self.__class__.__name__,
                     msg=PathValidatorException.MSG,
                     err_code=PathValidatorException.ERR_CODE,
-                    ex=validator_priming_result.exception,
-                )
-            )
-        # --- Cast the candidate into a Path for additional tests. ---#
-        path = cast(Path, candidate)
-        
-        # Handle the case that, the path's id gets flagged.
-        id_validation = self._toolkit.identity_service.validate_id(path.id)
-        if id_validation.is_failure:
-            # Send the exception chain on failure.
-            return ValidationResult.failure(
-                PathValidatorException(
-                    cls_mthd=method,
-                    cls_name=self.__class__.__name__,
-                    msg=PathValidatorException.MSG,
-                    err_code=PathValidatorException.ERR_CODE,
-                    ex=id_validation.exception,
-                )
-            )
-        # Handle the case that either the source or destination are not safe.
-        for square in [path.endpoints.to_list]:
-            square_validation_result = self._toolkit.square_validator.execute(square)
-            if square_validation_result.is_failure:
-                # Send the exception chain on failure.
-                return ValidationResult.failure(
-                    PathValidatorException(
-                        cls_mthd=method,
-                        cls_name=self.__class__.__name__,
-                        msg=PathValidatorException.MSG,
-                        err_code=PathValidatorException.ERR_CODE,
-                        ex=square_validation_result.exception,
-                    )
-                )
-        # Handle the case that, the origin and the destination are the same.
-        if path.endpoints.origin_is_destination:
-            # Send the exception chain on failure.
-            return ValidationResult.failure(
-                PathValidatorException(
-                    cls_mthd=method,
-                    cls_name=self.__class__.__name__,
-                    msg=PathValidatorException.MSG,
-                    err_code=PathValidatorException.ERR_CODE,
-                    ex=CircularPathException(
-                        cls_mthd=method,
-                        cls_name=self.__class__.__name__,
-                        msg=CircularPathException.MSG,
-                        err_code=CircularPathException.ERR_CODE,
-                    ),
+                    ex=certification.exception,
                 )
             )
         # --- Forward the work product to the caller. ---#
-        return ValidationResult.success(path)
-        
-        
+        return ValidationResult.success(
+            cast(
+                self.root_certifier.toolkit.model,
+                certification.payload
+            )
+        )
