3e5cad5c3c78dd2482e78cea37af0a815287525f
Banji Lawal <banji.k.lawal@gmail.com>
2026-08-22 22:54:16 -0500

    1) Wrote persistence.persist.PersistenceOperator

---
+++ src/persistence/__init__.py
@@ -0,0 +1,16 @@
+# src/persistence/__init__.py
+
+"""
+Module: persistence.__init__
+Author: Banji Lawal
+Created: 2026-04-03
+version: 0.0.2
+"""
+
+# =========== PERSISTENCE PACKAGE ===========#
+
+# Packages
+
+
+# Modules
+from .persist import PersistenceOperator
---
+++ src/persistence/persist.py
@@ -0,0 +1,16 @@
+# src/persistence/persist.py
+
+"""
+Module: persistence.persist
+Author: Banji Lawal
+Created: 2026-04-03
+version: 0.0.2
+"""
+
+from __future__ import annotations
+
+from abc import ABC
+
+
+class PersistenceOperator(ABC):
+    pass
