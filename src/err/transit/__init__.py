2b915310b49fa5c3cb0e80a313dfc20a68de6ac7
Banji Lawal <banji.k.lawal@gmail.com>
2026-08-24 17:37:30 -0500

    1) Wrote err.transit.TransitObjectException.2) Wrote TransitObjectException subclass, DispatcherException.

---
+++ src/err/transit/__init__.py
@@ -0,0 +1,16 @@
+# src/err/transit/__init__.py
+
+"""
+Module: err.transit.__init__
+Author: Banji Lawal
+Created: 2025-10-03
+version: 1.0.0
+"""
+
+# =========== ERR.TRANSIT PACKAGE ===========#
+
+# Packages
+from .dispatcher import *
+
+# Modules
+from .exception import TransitException
---
+++ src/err/transit/dispatcher/__init__.py
@@ -0,0 +1,16 @@
+# src/err/transit/dispatcher/__init__.py
+
+"""
+Module: err.transit.dispatcher.__init__
+Author: Banji Lawal
+Created: 2025-10-03
+version: 1.0.0
+"""
+
+# =========== ERR.TRANSIT.DISPATCHER PACKAGE ===========#
+
+# Packages
+
+
+# Modules
+from .exception import DispatcherException
