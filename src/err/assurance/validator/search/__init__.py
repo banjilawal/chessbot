72770bec4e27ea9552a58fb583098d388a28ba54
Banji Lawal <banji.k.lawal@gmail.com>
2026-08-25 00:36:02 -0500

    1) Wrote ChainContextValidator, VectorNodeContextValidator and DossierNodeContextValidator.

--- src/err/assurance/validator/search/__init__.py
+++ src/err/assurance/validator/search/__init__.py
@@ -10,6 +10,7 @@ version: 0.0.2
 # ============ ERR.ASSURANCE.VALIDATOR.SEARCH PACKAGE ===========#
 
 # Packages
+from .chain import *
 from .stack import *
 
 # Modules
---
+++ src/err/assurance/validator/search/chain/__init__.py
@@ -0,0 +1,17 @@
+# src/err/assurance/validator/search/chain/__init__.py
+
+"""
+Module: err.assurance.validator.search.chain.__init__
+Author: Banji Lawal
+Created: 2026-04-04
+version: 0.0.2
+"""
+
+# =========== ERR.ASSURANCE.VALIDATOR.SEARCH.CHAIN PACKAGE ===========#
+
+# Packages
+from .dossier import *
+from .vector import *
+
+# Modules
+from .exception import ChainContextValidatorException
