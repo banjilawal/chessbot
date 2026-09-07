ede3ac548bb33d44c79b492e8645e62432881107
Banji Lawal <banji.k.lawal@gmail.com>
2026-09-07 07:53:40 -0500

    1) Wrote HomeSquareNullException.

--- src/err/null/domain/model/state/square/__init__.py
+++ src/err/null/domain/model/state/square/__init__.py
@@ -10,7 +10,7 @@ version: 0.0.2
 # ============ ERR.NULL.DOMAIN.MODEL.STATE.SQUARE PACKAGE ===========#
 
 # Packages
-
+from .home import *
 
 # Modules
 from .exception import SquareNullException
---
+++ src/err/null/domain/model/state/square/home/__init__.py
@@ -0,0 +1,16 @@
+# src/err/null/domain/model/state/square/home/__init__.py
+
+"""
+Module: err.null.domain.model.state.square.home.__init__
+Author: Banji Lawal
+Created: 2026-04-04
+version: 0.0.2
+"""
+
+# ============ ERR.NULL.DOMAIN.MODEL.STATE.SQUARE PACKAGE ===========#
+
+# Packages
+
+
+# Modules
+from .exception import HomesSquareNullException
